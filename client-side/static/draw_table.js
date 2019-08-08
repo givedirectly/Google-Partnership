7845225f418a14be16768f201ac877f5fad185e4
juliexxia <juliexxia@google.com>
2019-08-07 20:02:23 -0400

    Run through intellij formatter

--- client-side/static/draw_table.js
+++ client-side/static/draw_table.js
@@ -1,40 +1,41 @@
 import {geoidTag, priorityTag, snapTag} from './script.js';
 
-export {drawTable as default };
+export {drawTable as default};
 
 /*
- * Draw a ranked table of the given features that have a SNAP ratio over the given
- * threshold. 
+ * Draw a ranked table of the given features that have a SNAP ratio over the
+ * given threshold.
  */
 function drawTable(features, povertyThreshold) {
-    const sortedAboveThreshold = features.filter(ee.Filter.gte(snapTag, povertyThreshold)).sort(priorityTag, false);
-    const headings = [geoidTag, priorityTag, snapTag]
-    const asListWithHeadings = ee.List(sortedAboveThreshold.iterate(
-        function(feature, list) {
-        	// Keep key order same as @const headings
-            return ee.List(list)
-            	.add([feature.get(geoidTag), feature.get(priorityTag), feature.get(snapTag)]);
-        },
-        [headings]));
-    asListWithHeadings.evaluate(function(success, failure) {
-        if (typeof failure !== 'undefined') {
-            // TODO(juliexxia): more robust error reporting
-            // https://developers.google.com/chart/interactive/docs/reference#errordisplay
-            console.log(failure);
-        } else {
-            const data = google.visualization.arrayToDataTable(success, false);
-            // Instantiate and draw the chart.
-    	    const table = new google.visualization.ChartWrapper({
-    			'chartType': 'Table',
-    			'containerId': 'table',
-    			'dataTable': data,
-    			'options': {
-    				'page': 'enable',
-    				'pageSize': 25,
-    			}
-			});
-            table.draw(); 
-            // console.timeEnd('update');  
+  const sortedAboveThreshold =
+      features.filter(ee.Filter.gte(snapTag, povertyThreshold))
+          .sort(priorityTag, false);
+  const headings = [geoidTag, priorityTag, snapTag] const asListWithHeadings =
+      ee.List(sortedAboveThreshold.iterate(function(feature, list) {
+        // Keep key order same as @const headings
+        return ee.List(list).add([
+          feature.get(geoidTag), feature.get(priorityTag), feature.get(snapTag)
+        ]);
+      }, [headings]));
+  asListWithHeadings.evaluate(function(success, failure) {
+    if (typeof failure !== 'undefined') {
+      // TODO(juliexxia): more robust error reporting
+      // https://developers.google.com/chart/interactive/docs/reference#errordisplay
+      console.log(failure);
+    } else {
+      const data = google.visualization.arrayToDataTable(success, false);
+      // Instantiate and draw the chart.
+      const table = new google.visualization.ChartWrapper({
+        'chartType': 'Table',
+        'containerId': 'table',
+        'dataTable': data,
+        'options': {
+          'page': 'enable',
+          'pageSize': 25,
         }
-    });
+      });
+      table.draw();
+      // console.timeEnd('update');
+    }
+  });
 }
--- client-side/static/update.js
+++ client-side/static/update.js
@@ -2,12 +2,13 @@ import run from './script.js';
 
 export {update as default};
 
-//TODO(juliexxia): figure out why pressing enter reloads the page but doesn't actually run
-// this update function.
+// TODO(juliexxia): figure out why pressing enter reloads the page but doesn't
+// actually run this update function.
 // Takes ~5 seconds to generate with 0.0 threshold.
 function update() {
   // console.time('update');
   const t = document.getElementById('threshold').value;
-  document.getElementById("current-threshold").innerHTML = "Current poverty threshold: " + t; 
+  document.getElementById('current-threshold').innerHTML =
+      'Current poverty threshold: ' + t;
   run(ee.Number.parse(t));
 }
