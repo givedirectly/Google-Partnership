0f79650df7b7dd521648bffa13d86aed88dd0b32
Janak Ramakrishnan <janakr@google.com>
2019-08-09 12:47:53 -0400

    do EE computation before chart loaded

--- client-side/static/script.js
+++ client-side/static/script.js
@@ -109,8 +109,7 @@ function updatePovertyThreshold(povertyThreshold) {
   const processedData =
       processJoinedData(joinedSnap, scalingFactor, povertyThreshold);
   addLayer(map, processedData.style({styleProperty: 'style'}), priorityLayerId);
-  google.charts.setOnLoadCallback(
-      () => drawTable(processedData, povertyThreshold));
+  drawTable(processedData, povertyThreshold);
 }
 
 // Main function that processes the data (FEMA damage, SNAP) and
