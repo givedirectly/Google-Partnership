961ab20caf1d5d95b05ca9c3f92e2531fcba9f82
Janak Ramakrishnan <janakr@google.com>
2019-08-12 15:27:07 -0400

    fix for push

--- cypress/integration/simple_test.js
+++ cypress/integration/simple_test.js
@@ -1,23 +1,22 @@
 const host = 'http://localhost:8081/';
 // Run via `yarn run cypress run`. Make sure you're using the right yarn!
-describe(
-    'Integration test',
-    () => {it('Draws a polygon', () => {
-      cy.visit(host);
-      const polygonButton = cy.get('[title="Draw a shape"]');
-      polygonButton.click();
-      // Wait for polygon selection overlay to appear.
-      // Fragile, but ensures that "clicking" layer is present.
-      cy.get(
-          'div[style*="cursor: url(\\"https://maps.gstatic.com/mapfiles/crosshair.cur\\") 7 7, crosshair;"]');
-      drawPointAndPrepareForNext(50, 250, 0);
-      drawPointAndPrepareForNext(400, 50);
-      drawPointAndPrepareForNext(450, 150);
-      drawPointAndPrepareForNext(50, 250);
-      // Is the draggable edge present?
-      cy.get('div[style*="left: -100px; top: -95px;"');
-    });
+describe('Integration test', () => {
+  it('Draws a polygon', () => {
+    cy.visit(host);
+    const polygonButton = cy.get('[title="Draw a shape"]');
+    polygonButton.click();
+    // Wait for polygon selection overlay to appear.
+    // Fragile, but ensures that "clicking" layer is present.
+    cy.get(
+        'div[style*="cursor: url(\\"https://maps.gstatic.com/mapfiles/crosshair.cur\\") 7 7, crosshair;"]');
+    drawPointAndPrepareForNext(50, 250, 0);
+    drawPointAndPrepareForNext(400, 50);
+    drawPointAndPrepareForNext(450, 150);
+    drawPointAndPrepareForNext(50, 250);
+    // Is the draggable edge present?
+    cy.get('div[style*="left: -100px; top: -95px;"');
   });
+});
 
 /**
  * Draws a point on the map and mouses over to simulate moving to the next one.
