3115ac8a311bde28b46c40eb7ed7f3f8755eef90
Janak Ramakrishnan <janakr@google.com>
2019-08-12 15:22:15 -0400

    Update push hook to include tests, add test that polygon drawing works

--- cypress/integration/simple_test.js
+++ cypress/integration/simple_test.js
@@ -14,15 +14,14 @@ describe('Integration test',() => {
     drawPointAndWait(50, 250, 0);
     drawPointAndWait(400, 50);
     drawPointAndWait(450, 150);
-    // This last wait won't actually wait because it's the first point on the
-    // map, so we verify that there's a drag node present.
     drawPointAndWait(50, 250);
+    // Is the draggable edge present?
     cy.get('div[style*="left: -100px; top: -95px;"');
   })
 });
 
 function drawPointAndWait(x, y) {
   cy.get('.map').click(x, y);
-  // cy.wait(200);
-  cy.get('div[style*="left: ' + (x - 325) + 'px; top: ' + (y - 245) + 'px;"');
+  // mouse-over on map to simulate moving to next point seems necessary.
+  cy.get('.map').trigger('mouseover');
 }
