b24bfa9a1ca0d0ca6f2d49d233bc298198653c4b
Janak Ramakrishnan <janakdr@users.noreply.github.com>
2019-08-12 17:51:03 -0400

    Add test that polygon can be drawn on map (#46)
    Sadly, seems to require waiting for the page to register clicks, will investigate later.

--- cypress/integration/simple_test.js
+++
@@ -1,6 +0,0 @@
-// Run via `yarn run cypress run`. Make sure you're using the right yarn!
-describe('Integration test',() => {
-  it('Opens the page', () => {
-    cy.visit('http://localhost:8080/');
-  })
-});
