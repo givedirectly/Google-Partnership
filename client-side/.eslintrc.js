1fcf667ea7a5ea151d1d821e63c83025d512ea56
Janak Ramakrishnan <janakdr@users.noreply.github.com>
2019-08-12 12:10:06 -0400

    Add eslint configuration and lint script.js (#42)
    There are some annoying bugs around minor issues, but this should catch a lot. Install by running yarn add eslint and yarn add eslint-config-google then running yarn run eslint path/to/file.

---
+++ client-side/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'rules': {
+    'new-cap': [2, {
+      capIsNewExceptionPattern: 'ee\..*',
+      // TODO(janakr): for some reason ee\n    .Feature isn't matched.
+      capIsNewExceptions: ['Feature']}],
+    // TODO(https://github.com/google/eslint-config-google/issues/58):
+    'indent': ['error', 2, {'CallExpression': {'arguments': 2}, 'ignoredNodes': ['CallExpression > CallExpression', 'CallExpression > MemberExpression']}]
+  },
+};
