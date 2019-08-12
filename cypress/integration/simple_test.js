0bc0d57997daf32a1f1657d6439675fe9fd18a71
Janak Ramakrishnan <janakr@google.com>
2019-08-12 15:26:55 -0400

    fix for push

--- cypress/integration/simple_test.js
+++ cypress/integration/simple_test.js
@@ -10,15 +10,22 @@ describe(
       // Fragile, but ensures that "clicking" layer is present.
       cy.get(
           'div[style*="cursor: url(\\"https://maps.gstatic.com/mapfiles/crosshair.cur\\") 7 7, crosshair;"]');
-      drawPointAndWait(50, 250, 0);
-      drawPointAndWait(400, 50);
-      drawPointAndWait(450, 150);
-      drawPointAndWait(50, 250);
+      drawPointAndPrepareForNext(50, 250, 0);
+      drawPointAndPrepareForNext(400, 50);
+      drawPointAndPrepareForNext(450, 150);
+      drawPointAndPrepareForNext(50, 250);
       // Is the draggable edge present?
       cy.get('div[style*="left: -100px; top: -95px;"');
-    })});
+    });
+  });
 
-function drawPointAndWait(x, y) {
+/**
+ * Draws a point on the map and mouses over to simulate moving to the next one.
+ *
+ * @param {number} x x-coordinate of the point in Cypress's scheme.
+ * @param {number} y y-coordinate of the point in Cypress's scheme.
+ */
+function drawPointAndPrepareForNext(x, y) {
   cy.get('.map').click(x, y);
   // mouse-over on map to simulate moving to next point seems necessary.
   cy.get('.map').trigger('mouseover');
