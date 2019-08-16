2f6633e66a9034e9e9a22ca5520a44efbee3d38b
juliexxia <juliexxia@google.com>
2019-08-16 12:18:16 -0400

    tidy

--- cypress/integration/update_integration_test.js
+++ cypress/integration/update_integration_test.js
@@ -1,22 +1,22 @@
 const host = 'http://localhost:8080/';
 
+/** Checks correct initial value of poverty threshold and checks it ' +
+ 'updates when updated with valid value */
 describe('Integration test', () => {
-  it('Checks correct initial value of poverty threshold and checks it ' +
-         'updates when updated with valid value',
-     () => {
-       cy.visit(host);
+  it('Checks new valid value', () => {
+    cy.visit(host);
 
-       // Assert initial text is set to default threshold of 0.3
-       cy.get('#current-threshold')
-           .should('have.text', 'Current poverty threshold: 0.3');
+    // Assert initial text is set to default threshold of 0.3
+    cy.get('#current-threshold')
+       .should('have.text', 'Current poverty threshold: 0.3');
 
-       cy.get('#threshold').type('0.5');
-       cy.get('#update-button').click();
+    cy.get('#threshold').type('0.5');
+    cy.get('#update-button').click();
 
-       cy.get('#current-threshold')
-           .should('have.text', 'Current poverty threshold: 0.5');
-       cy.get('#threshold-error-message').should('have.text', '');
-     });
+    cy.get('#current-threshold')
+       .should('have.text', 'Current poverty threshold: 0.5');
+    cy.get('#threshold-error-message').should('have.text', '');
+  });
 });
 
 describe('Integration test', () => {
@@ -32,15 +32,14 @@ describe('Integration test', () => {
 
 describe('Integration test', () => {
   it('Checks error message dissapears when bad threshold value ' +
-         'replaced by valid value',
-     () => {
-       cy.visit(host);
+         'replaced by valid value', () => {
+    cy.visit(host);
 
-       cy.get('#update-button').click();
+    cy.get('#update-button').click();
 
-       cy.get('#threshold').type('0.5');
-       cy.get('#update-button').click();
+    cy.get('#threshold').type('0.5');
+    cy.get('#update-button').click();
 
-       cy.get('#threshold-error-message').should('have.text', '');
-     });
+    cy.get('#threshold-error-message').should('have.text', '');
+  });
 });
