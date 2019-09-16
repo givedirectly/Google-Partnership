7a8e6be79078797748e037583eb9fb16cdffeab4
ramv13 <ramsai.vellanki@gmail.com>
2019-09-16 03:07:45 -0400

    Updating documentation for setLoading

--- client-side/static/loading.js
+++ client-side/static/loading.js
@@ -1,8 +1,8 @@
 export {setLoading};
 
 /**
- * Takes an id of a div and updates that div's loading state (i.e. adds or
- * removes the loader overlay).
+ * Takes an id of a div and updates that div's loading state (i.e. shows or
+ * hides the loader overlay).
  *
  * @param {Number} divId The id of the div that needs to update loading state
  * @param {boolean} loading True iff the div is currently reloading content
