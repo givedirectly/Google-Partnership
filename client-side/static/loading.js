22f96587c8dd40d3102e1eb757b1c34721cd63f4
ramv13 <ramsai.vellanki@gmail.com>
2019-09-21 19:27:52 -0400

    Removing dynamic loading overlay creation

--- client-side/static/loading.js
+++ client-side/static/loading.js
@@ -9,20 +9,7 @@ export {setLoading};
  */
 function setLoading(divId, loading) {
   let overlay = document.getElementById(divId + '-loader');
-
-  // Create the overlay if it does not exist already.
-  if (!overlay) {
-    const primaryDiv = document.getElementById(divId);
-    if (!primaryDiv) return;
-
-    overlay = document.createElement('div');
-    overlay.id = divId + '-loader';
-    overlay.className = 'loader';
-    overlay.innerHTML = '<div class="ellipsis"><div></div><div></div><div>' +
-        '</div><div></div></div>';
-
-    primaryDiv.appendChild(overlay);
-  }
+  if (!overlay) return;
 
   if (loading) {
     overlay.style.opacity = 1;
