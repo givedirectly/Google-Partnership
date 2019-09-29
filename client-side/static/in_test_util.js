c9b40aad5a896d8f4fe966d6c2d9d2d37912a36a
Janak Ramakrishnan <janakr@janakr-macbookpro2.roam.corp.google.com>
2019-09-29 16:43:01 -0400

    Log in when not in test

---
+++ client-side/static/in_test_util.js
@@ -0,0 +1,10 @@
+export {inProduction as default};
+
+function getCookieValue(cookieName) {
+  const value = document.cookie.match('(^|[^;]+)\\s*' + cookieName + '\\s*=\\s*([^;]+)');
+  return value ? value.pop() : '';
+}
+
+function inProduction() {
+  return !getCookieValue('IN_CYPRESS_TEST');
+}
--- client-side/static/script.js
+++ client-side/static/script.js
@@ -1,6 +1,7 @@
 import createError from './create_error.js';
 import createMap from './create_map.js';
 import run from './run.js';
+import inProduction from './in_test_util.js';
 
 export {map};
 
@@ -48,15 +49,18 @@ function setup() {
       });
     };
 
-    // Attempt to authenticate using existing credentials.
-    // TODO(#21): Fix buggy authentification.
-    // ee.data.authenticate(
-    //     CLIENT_ID,
-    //     runOnSuccess,
-    //     createError('authenticating'),
-    //     null,
-    //     onImmediateFailed);
-    runOnSuccess();
+    if (inProduction()) {
+      // Attempt to authenticate using existing credentials.
+      ee.data.authenticate(
+          CLIENT_ID,
+          runOnSuccess,
+          createError('authenticating'),
+          null,
+          onImmediateFailed);
+    } else {
+      // TODO(#21): have something better for tests.
+      runOnSuccess();
+    }
   });
 }
 
