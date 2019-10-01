abebcfae3ed3d186c4bd9d356d1711cd88a15d5b
Janak Ramakrishnan <janakr@google.com>
2019-10-01 14:24:21 -0400

    back to master to do import

--- cypress/integration/unit_tests/checkbox_test.js
+++ cypress/integration/unit_tests/checkbox_test.js
@@ -8,19 +8,18 @@ const mockOverlay = {};
 
 describe('Unit test for toggleLayerOn', () => {
   beforeEach(() => {
-    layerMap['asset0'] = new LayerMapValue(0, true);
-    layerMap['asset1'] = new LayerMapValue(1, false);
-    layerMap['asset2'] = new LayerMapValue(2, false);
+    layerMap['asset0'] = new LayerMapValue(mockOverlay, 0, true);
+    layerMap['asset1'] = new LayerMapValue(mockOverlay, 1, false);
+    layerMap['asset2'] = new LayerMapValue(null, 2, false);
   });
 
   it('displays a hidden but loaded layer', () => {
     expect(layerMap['asset1'].displayed).to.equals(false);
-    expect(layerMap['asset1'].overlay).to.not.be.null;
+    expect(layerMap['asset1'].data).to.not.be.null;
 
     const overlayMapTypesApi = {
       setAt: (index, overlay) => {},
     };
-    const map = {overlayMapTypes: overlayMapTypesApi};
     const mockOverlayMapTypes = Cypress.sinon.mock(overlayMapTypesApi);
 
     mockOverlayMapTypes.expects('setAt').once().withArgs(1, mockOverlay);
@@ -31,12 +30,11 @@ describe('Unit test for toggleLayerOn', () => {
 
   it('loads a hidden layer and displays', () => {
     expect(layerMap['asset2'].displayed).to.equals(false);
-    expect(layerMap['asset2'].overlay).to.be.null;
+    expect(layerMap['asset2'].data).to.be.null;
 
     const overlayMapTypesApi = {
       setAt: (index, overlay) => {},
     };
-    const map = {overlayMapTypes: overlayMapTypesApi};
     const mockOverlayMapTypes = Cypress.sinon.mock(overlayMapTypesApi);
 
     // Oddly, if you print layerMap['asset2'] to console here, and also print
@@ -54,17 +52,16 @@ describe('Unit test for toggleLayerOn', () => {
 
     mockOverlayMapTypes.verify();
     expect(layerMap['asset2'].displayed).to.equals(true);
-    expect(layerMap['asset2'].overlay).to.not.be.null;
+    expect(layerMap['asset2'].data).to.not.be.null;
   });
 
   it('check hidden layer, then uncheck before getMapCallback', () => {
     expect(layerMap['asset2'].displayed).to.equals(false);
-    expect(layerMap['asset2'].overlay).to.be.null;
+    expect(layerMap['asset2'].data).to.be.null;
 
     const overlayMapTypesApi = {
       setAt: (index, overlay) => {},
     };
-    const map = {overlayMapTypes: overlayMapTypesApi};
     const mockOverlayMapTypes = Cypress.sinon.mock(overlayMapTypesApi);
 
     mockOverlayMapTypes.expects('setAt').once().withArgs(2, null);
@@ -75,19 +72,18 @@ describe('Unit test for toggleLayerOn', () => {
 
     mockOverlayMapTypes.verify();
     expect(layerMap['asset2'].displayed).to.equals(false);
-    expect(layerMap['asset2'].overlay).to.not.be.null;
+    expect(layerMap['asset2'].data).to.not.be.null;
   });
 });
 
 describe('Unit test for toggleLayerOff', () => {
   it('hides a displayed layer', () => {
     expect(layerMap['asset0'].displayed).to.equals(true);
-    expect(layerMap['asset0'].overlay).to.not.be.null;
+    expect(layerMap['asset0'].data).to.not.be.null;
 
     const overlayMapTypesApi = {
       setAt: (index, overlay) => {},
     };
-    const map = {overlayMapTypes: overlayMapTypesApi};
     const mockOverlayMapTypes = Cypress.sinon.mock(overlayMapTypesApi);
 
     mockOverlayMapTypes.expects('setAt').once().withArgs(0, null);
