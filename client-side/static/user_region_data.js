34bbca901f0cab41f628d5ad224ee85c6a599ff8
Julie <30670028+juliexxia@users.noreply.github.com>
2019-10-03 13:16:36 -0400

    Use a custom popup to have custom close popup logic (#89)

---
+++ client-side/static/user_region_data.js
@@ -0,0 +1,9 @@
+export {userRegionData};
+
+/**
+ * Map from Google Maps Polygon to ShapeData, so that on user-region
+ * modifications we can track new notes values and write data back to database.
+ * Data is added to this map on loading from the backend or on user creation,
+ * and removed if the polygon is deleted.
+ */
+const userRegionData = new Map();
