e26a563a550cc82c4df10a38386bc2b90abfc196
Janak Ramakrishnan <janakr@google.com>
2019-10-04 14:24:58 -0400

    fixed test

--- test/integration_tests/checkbox_test.js
+++ test/integration_tests/checkbox_test.js
@@ -1,17 +1,18 @@
 const {By} = require('selenium-webdriver');
 const chai = require('chai');
-const testUtil = require('../lib/test_support');
+const testSupport = require('../lib/test_support');
 
 describe('Integration test', function() {
-  const driverPromise = testUtil.setUp(this);
+  const driverPromise = testSupport.setUp(this);
 
   it('Checks threshold update checks score box', async () => {
-    const driver = await testUtil.loadPage(driverPromise);
+    const driver = await testSupport.loadPage(driverPromise);
     driver.findElement(By.id('score')).click();
     const result = await driver.findElement(By.id('score')).isSelected();
     chai.expect(result).to.be.false;
     driver.findElement(By.id('poverty threshold')).sendKeys('1.0');
     driver.findElement(By.id('update')).click();
+    await testSupport.waitForLoad(driver);
     const nextResult = await driver.findElement(By.id('score')).isSelected();
     chai.expect(nextResult).to.be.true;
   });
