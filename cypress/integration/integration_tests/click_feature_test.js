3bd89c2e7cd581c04d11b34be60ee373287301d5
Janak Ramakrishnan <janakr@google.com>
2019-10-09 18:36:54 -0400

    fixes

--- cypress/integration/integration_tests/click_feature_test.js
+++ cypress/integration/integration_tests/click_feature_test.js
@@ -41,11 +41,16 @@ describe('Integration test for clicking feature', () => {
 
   // Ensures that listeners are cleared when table instance and data
   // are updated.
-  it('click highlights correct feature even after update', () => {
+  it.only('click highlights correct feature even after update', () => {
     cy.visit(host);
     cy.awaitLoad();
 
     cy.get('.map').click(343, 184);
+    cy.get('.google-visualization-table-tr-sel')
+        .find('[class="google-visualization-table-td"]')
+        .should(
+            'have.text',
+            'Block Group 4, Census Tract 2511, Harris County, Texas67019');
     cy.get('[id="damage threshold"]').type('0.9');
     cy.get('[id="update"]').click();
     cy.get('.google-visualization-table-tr-sel')
