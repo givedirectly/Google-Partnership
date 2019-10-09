43d89a77f2b7feeafb8af3e6f60fb4fda224ea0c
Ruth Talbot <ruthtalbot@ruthtalbot-macbookpro.roam.corp.google.com>
2019-10-09 14:21:37 -0400

    formatting fixes

--- client-side/static/earth_engine_asset.js
+++ client-side/static/earth_engine_asset.js
@@ -3,43 +3,71 @@ export {assets};
 /* EarthEngineAsset class to store relevant data about any assets, including
  * special coloring instructions and display name. */
 class EarthEngineAsset {
-  constructor(type, displayName, displayOnLoad, visParams, colorFunction) {
+  /**
+   * @param {string} type
+   * @param {string} displayName
+   * @param {boolean} displayOnLoad
+   * @param {Function} colorFunction
+   **/
+  constructor(type, displayName, displayOnLoad, colorFunction) {
     this.type = type;
     this.displayName = displayName;
     this.displayOnLoad = displayOnLoad;
-    this.visParams = visParams;
     this.colorFunction = colorFunction;
   }
 
-  /* @return {boolean} Whether to display the asset on initial load. */
+  /**
+   * Returns whether to display the asset on initial load.
+   * @return {boolean}
+   */
   shouldDisplayOnLoad() {
     return this.displayOnLoad;
   }
 
-  /* @return {Function} The function to color the asset. Null if asset doesn't
-   * need styling or is styled elsewhere. */
+  /**
+   * Returns the function to color the asset. Null if asset doesn't
+   * need styling or is styled elsewhere.
+   *
+   * @return {Function}
+   */
   getColorFunction() {
     return this.colorFunction;
   }
 
-  /* @return {string} The asset type. */
+  /**
+   * Returns the asset type.
+   *
+   * @return {string} The asset type.
+   */
   getType() {
     return this.type;
   }
 
-  /* @return {string} The display name for the asset. */
+  /**
+   * Return the display name for the asset.
+   *
+   * @return {string}
+   */
   getDisplayName() {
     return this.displayName;
   }
 }
 
-/* @param {GeoJSON.Feature} feature */
+/**
+ * Color the feature with SVI specific logic.
+ *
+ * @param {GeoJSON.Feature} feature
+ */
 function colorSVILayer(feature) {
   const opacity = Math.min(Math.round(255 * feature.properties['SVI']), 255);
   feature.properties['color'] = [255 - opacity, 0, 255 - opacity, opacity];
 }
 
-/* @param {GeoJSON.Feature} feature */
+/**
+ * Color the feature with Path of Storm Radii specific logic.
+ *
+ * @param {GeoJSON.Feature} feature
+ */
 function colorPathofStormRadiiLayer(feature) {
   const opacity =
       Math.min(Math.round(255 * feature.properties['RADII'] / 100), 255);
