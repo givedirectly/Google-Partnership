57d12f456123821df373c439f4d64da2d2965c82
ramv13 <ramsai.vellanki@gmail.com>
2019-10-08 21:31:05 -0400

    Fixing integration tests

--- test/integration_tests/sidebar_test.js
+++ test/integration_tests/sidebar_test.js
@@ -3,64 +3,66 @@ describe('Integration test for the sidebar', function() {
 
   it('enables toggling', async () => {
     const driver = await loadPage(driverPromise);
-    debugger
+
     // The sidebar should be collapsed to start.
     await driver.findElement({
-      xpath: '//div[@id="sidebar"][contains(@style,"width: 64px")]',
+      xpath: '//div[@id="sidebar"][not(contains(@style,"width: 25%"))]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-thresholds"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-thresholds"]',//[contains(@style,"display: none")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-datasets"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-datasets"]',//[contains(@style,"display: none")]',
     });
 
     // The thresholds should appear on toggling.
-    driver.findElement({id: 'sidebar-toggle-thresholds'}).click();
+    await driver.findElement({id: 'sidebar-toggle-thresholds'}).click();
     await driver.findElement({
       xpath: '//div[@id="sidebar"][contains(@style,"width: 25%")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-thresholds"][contains(@style,"display: block")]',
+      xpath: '//div[@id="sidebar-thresholds"][not(contains(@style,"display: none"))]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-datasets"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-datasets"]',//[contains(@style,"display: none")]',
     });
 
     // The thresholds sidebar should disappear on toggling.
-    driver.findElement({id: 'sidebar-toggle-thresholds'}).click();
+    await driver.findElement({id: 'sidebar-toggle-thresholds'}).click();
     await driver.findElement({
-      xpath: '//div[@id="sidebar"][contains(@style,"width: 64px")]',
+      xpath: '//div[@id="sidebar"][not(contains(@style,"width: 25%"))]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-thresholds"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-thresholds"]',//[contains(@style,"display: none")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-datasets"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-datasets"]',//[contains(@style,"display: none")]',
     });
 
     // The datasets should appear on toggling.
-    driver.findElement({id: 'sidebar-toggle-datasets'}).click();
+    await driver.findElement({id: 'sidebar-toggle-datasets'}).click();
     await driver.findElement({
       xpath: '//div[@id="sidebar"][contains(@style,"width: 25%")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-thresholds"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-thresholds"]',//[contains(@style,"display: none")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-datasets"][contains(@style,"display: block")]',
+      xpath: '//div[@id="sidebar-datasets"][not(contains(@style,"display: none"))]',
     });
 
+
+    await driver.sleep(1000000);
     // The thresholds should appear on toggling even from another sidebar view.
     driver.findElement({id: 'sidebar-toggle-thresholds'}).click();
     await driver.findElement({
       xpath: '//div[@id="sidebar"][contains(@style,"width: 25%")]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-thresholds"][contains(@style,"display: block")]',
+      xpath: '//div[@id="sidebar-thresholds"][not(contains(@style,"display: none"))]',
     });
     await driver.findElement({
-      xpath: '//div[@id="sidebar-datasets"][contains(@style,"display: none")]',
+      xpath: '//div[@id="sidebar-datasets"]',//[contains(@style,"display: none")]',
     });
   });
 });
