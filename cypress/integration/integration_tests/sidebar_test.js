bbd24e6b0c6345765bce0baa317edd25e158620c
ramv13 <ramsai.vellanki@gmail.com>
2019-10-10 18:43:45 -0400

    Fixing tests

--- cypress/integration/integration_tests/sidebar_test.js
+++ cypress/integration/integration_tests/sidebar_test.js
@@ -4,32 +4,32 @@ describe('Integration test for the sidebar', () => {
     cy.awaitLoad();
 
     // The sidebar should be collapsed to start.
-    cy.get('#sidebar').should('have.css', 'width').and('eq', '64px');
-    cy.get('#sidebar-thresholds').should('have.css', 'display').and('eq', 'none');
-    cy.get('#sidebar-datasets').should('have.css', 'display').and('eq', 'none');
+    cy.get('#sidebar').should('have.css', 'width', '64px');
+    cy.get('#sidebar-thresholds').should('have.css', 'display', 'none');
+    cy.get('#sidebar-datasets').should('have.css', 'display', 'none');
 
     // The thresholds should appear on toggling.
     cy.get('#sidebar-toggle-thresholds').click();
-    cy.get('#sidebar').should('have.css', 'width').and('eq', '25%');
-    cy.get('#sidebar-thresholds').should('have.css', 'display').and('neq', 'none');
-    cy.get('#sidebar-datasets').should('have.css', 'display').and('eq', 'none');
+    cy.get('#sidebar').should('have.css', 'width', '271px');
+    cy.get('#sidebar-thresholds').should('not.have.css', 'display', 'none');
+    cy.get('#sidebar-datasets').should('have.css', 'display', 'none');
 
     // The thresholds sidebar should disappear on toggling.
     cy.get('#sidebar-toggle-thresholds').click();
-    cy.get('#sidebar').should('have.css', 'width').and('eq', '64px');
-    cy.get('#sidebar-thresholds').should('have.css', 'display').and('eq', 'none');
-    cy.get('#sidebar-datasets').should('have.css', 'display').and('eq', 'none');
+    cy.get('#sidebar').should('have.css', 'width', '64px');
+    cy.get('#sidebar-thresholds').should('have.css', 'display', 'none');
+    cy.get('#sidebar-datasets').should('have.css', 'display', 'none');
 
     // The datasets should appear on toggling.
     cy.get('#sidebar-toggle-datasets').click();
-    cy.get('#sidebar').should('have.css', 'width').and('eq', '25%');
-    cy.get('#sidebar-thresholds').should('have.css', 'display').and('eq', 'none');
-    cy.get('#sidebar-datasets').should('have.css', 'display').and('neq', 'none');
+    cy.get('#sidebar').should('have.css', 'width', '271px');
+    cy.get('#sidebar-thresholds').should('have.css', 'display', 'none');
+    cy.get('#sidebar-datasets').should('not.have.css', 'display', 'none');
 
     // The thresholds should appear on toggling even from another sidebar view.
     cy.get('#sidebar-toggle-thresholds').click();
-    cy.get('#sidebar').should('have.css', 'width').and('eq', '25%');
-    cy.get('#sidebar-thresholds').should('have.css', 'display').and('neq', 'none');
-    cy.get('#sidebar-datasets').should('have.css', 'display').and('eq', 'none');
+    cy.get('#sidebar').should('have.css', 'width', '271px');
+    cy.get('#sidebar-thresholds').should('not.have.css', 'display', 'none');
+    cy.get('#sidebar-datasets').should('have.css', 'display', 'none');
   });
 });
