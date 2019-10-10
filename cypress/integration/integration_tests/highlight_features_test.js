9b2749f1728c704327af454d8482eced8f7472a7
Janak Ramakrishnan <janakdr@users.noreply.github.com>
2019-10-10 09:52:24 -0400

    Revert "Migrate highlight_features and update integration tests to Selenium (#97)" (#113)
    This reverts commit b22b3ba.
    
    Changed update_test.js to clear() the fields before it sent keys, since I think that was always causing problems and became even more of an issue with deck.gl, maybe.
    
    Work towards #107.

---
+++ cypress/integration/integration_tests/highlight_features_test.js
@@ -0,0 +1,30 @@
+describe('Integration tests for highlighting chosen districts', () => {
+  it('Clicks on list and highlights district', () => {
+    cy.visit(host);
+    cy.awaitLoad();
+
+    // Actually verifying that the element appears is difficult, because the
+    // drawing happens on a canvas, which doesn't expose its contents. So we
+    // don't do it, instead just verifying that nothing terrible happens.
+    getTable().get('tr').eq(1).click();
+    // Shift-click to select a range.
+    cy.get('body').type('{shift}', {release: false, force: true});
+    getTable().get('tr').eq(3).click();
+    // Clear shift modifier. Unfortunately this seems to swallow the next
+    // click.
+    cy.get('body').type('{shift}').click({force: true});
+    // TODO(janakr): this click isn't registered properly. But the next one
+    // is.
+    getTable().get('tr').eq(2).click();
+    getTable().get('tr').eq(2).click();
+  });
+});
+
+/**
+ * Gets the table with the ranked list.
+ *
+ * @return {Object}
+ */
+function getTable() {
+  return cy.get(tableClass);
+}
---
+++ cypress/integration/integration_tests/update_test.js
@@ -0,0 +1,37 @@
+describe('Integration test for update.js', () => {
+  /**
+   * Checks that by setting the threshold to 100% the list and page number
+   * empty. This is somewhat brittle in that if we ever worked with some poverty
+   * data that did have 100% poverty in any areas, this would break.
+   *
+   * On conditional testing:
+   * https://docs.cypress.io/guides/core-concepts/conditional-testing.html#Definition
+   */
+  // Has been historically disabled for flakiness
+  //    CypressError: Timed out retrying: Expected to find element:
+  //    '.google-visualization-table-page-numbers', but never found it.
+
+  it('Checks list size updates with higher threshold', () => {
+    cy.visit(host);
+    cy.awaitLoad();
+
+    cy.get('[id="damage threshold"]').clear().type('0.0');
+    cy.get('[id="update"]').click();
+
+    const numPages = cy.get('.google-visualization-table-page-numbers')
+                         .find('*')
+                         .then((elm) => elm.length);
+    numPages.should('gt', 0);
+
+    cy.get('[id="poverty threshold"]').clear().type('1.0');
+    cy.get('[id="damage threshold"]').clear().type('1.0');
+    cy.get('[id="update"]').click();
+
+    cy.get(tableClass)
+        .find('.google-visualization-table-page-numbers')
+        .should('not.exist');
+    cy.get(tableClass)
+        .find('.google-visualization-table-tr-even')
+        .should('not.exist');
+  });
+});
