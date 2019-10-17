23890473d8d002ae88ab54c2ca2cf0ced78cdfe8
Janak Ramakrishnan <janakr@google.com>
2019-10-17 15:15:26 -0400

    pushing

--- client-side/static/import/add_asset.js
+++ client-side/static/import/add_asset.js
@@ -56,9 +56,8 @@ function enableWhenReady() {
 ee.data.setCloudApiEnabled(true);
 
 // Perform EE login/Google OAuth2 process.
-const authenticator =
-    new Authenticator(
-        setUpAllHeaders, onStartupTaskCompleted, setStatusDiv, [storageScope]);
+const authenticator = new Authenticator(
+    setUpAllHeaders, onStartupTaskCompleted, setStatusDiv, [storageScope]);
 authenticator.start();
 
 /**
