7c7cc61a03345439ba8d5760d7f0497df0145e73
Janak Ramakrishnan <janakdr@users.noreply.github.com>
2019-10-22 15:18:16 -0400

    Rephrase Popup class using ES6 classes (#148)
    We can't pull out to the top level apparently because the map class isn't always available (or there are some duplicate definitions, unclear), but at least this looks more like a class.

--- client-side/static/polygon_draw.js
+++ client-side/static/polygon_draw.js
@@ -2,7 +2,7 @@ import createError from './create_error.js';
 import {mapContainerId, writeWaiterId} from './dom_constants.js';
 import {getTestCookie, inProduction} from './in_test_util.js';
 import {addLoadingElement, loadingElementFinished} from './loading.js';
-import {addPopUpListener, createPopup, setUpPopup, updateDamage} from './popup.js';
+import {createPopup, setUpPopup} from './popup.js';
 import {getResources} from './resources.js';
 import {userRegionData} from './user_region_data.js';
 
@@ -191,8 +191,7 @@ function setUpPolygonDrawing(map, firebasePromise) {
       const data = new ShapeData(null, '', 'calculating');
       userRegionData.set(polygon, data);
       const popup = createPopup(polygon, map);
-      addPopUpListener(popup);
-      data.update(polygon, (damage) => updateDamage(popup, damage), '');
+      data.update(polygon, (damage) => popup.updateDamage(damage), '');
     });
 
     drawingManager.setMap(map);
@@ -243,7 +242,7 @@ function drawRegionsFromFirestoreQuery(querySnapshot, map) {
         new ShapeData(
             userDefinedRegion.id, userDefinedRegion.get('notes'),
             userDefinedRegion.get('damage')));
-    addPopUpListener(createPopup(polygon, map));
+    createPopup(polygon, map);
     polygon.setMap(map);
   });
   loadingElementFinished(mapContainerId);
