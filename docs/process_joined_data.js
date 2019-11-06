42aee6ee1b48dc58ee8d4a2f3421ad69efd3f9fa
Janak Ramakrishnan <janakr@google.com>
2019-11-06 16:10:55 -0500

    firebase-layers

--- docs/process_joined_data.js
+++ docs/process_joined_data.js
@@ -59,7 +59,7 @@ function colorAndRate(
  * @param {number} povertyWeight float between 0 and 1 that describes what
  *     percentage of the score should be based on poverty (this is also a proxy
  *     for damageWeight which is 1-this value).
- * @return {Promise} Promise with resolved data
+ * @return {Promise}
  */
 function processJoinedData(
     dataPromise, scalingFactor, povertyThreshold, damageThreshold,
