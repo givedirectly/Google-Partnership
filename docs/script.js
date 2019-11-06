b3b742d95179d37de94ba9ce12f8d40105cee95b
Janak Ramakrishnan <janakr@google.com>
2019-11-06 15:56:22 -0500

    firebase-layers

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,10 @@
 import {authenticateToFirebase, Authenticator, CLIENT_ID, initializeEE, initializeFirebase} from './authenticate.js';
 import createMap from './create_map.js';
 import {earthEngineTestTokenCookieName, firebaseTestTokenCookieName, getCookieValue, inProduction} from './in_test_util.js';
+import {getDisaster, getResources} from './resources.js';
 import run from './run.js';
 import SettablePromise from './settable_promise.js';
 import {initializeSidebar} from './sidebar.js';
-import {getDisaster, getResources} from './resources.js';
 
 // The base Google Map, Initialized lazily to ensure doc is ready
 let map = null;
