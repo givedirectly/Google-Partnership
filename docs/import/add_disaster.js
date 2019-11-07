7f818906c47ab1b8b40b1947e23f24cd66fb9a62
juliexxia <juliexxia@google.com>
2019-11-07 10:57:29 -0500

    auth stuff

--- docs/import/add_disaster.js
+++ docs/import/add_disaster.js
@@ -1,4 +1,26 @@
-export {addDisaster};
+import {authenticateToFirebase, Authenticator} from '../authenticate.js';
+import TaskAccumulator from './task_accumulator.js';
+import SettablePromise from '../settable_promise';
+
+export {addDisaster, taskAccumulator};
+
+function enableWhenReady() {
+
+}
+
+// Necessary for listAssets.
+ee.data.setCloudApiEnabled(true);
+
+// 2 tasks: EE authentication,
+const taskAccumulator = new TaskAccumulator(2, enableWhenReady);
+// TODO: do something with this promise, pass it somewhere - probably once tagging happens.
+const firebaseAuthPromise = new SettablePromise();
+const authenticator = new Authenticator(
+    (token) => firebaseAuthPromise.setPromise(authenticateToFirebase(token)),
+    () => taskAccumulator.taskCompleted());
+authenticator.start();
 
 /** TODO: this doc */
-function addDisaster() {}
+function addDisaster(
+
+) {}
