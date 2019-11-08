2d80552bdc9c7790080d87f50ef3432c4a757a34
Janak Ramakrishnan <janakr@google.com>
2019-11-08 18:10:42 -0500

    fix-center

---
+++ docs/firestore_document.js
@@ -0,0 +1,34 @@
+import {getTestCookie, inProduction} from './in_test_util.js';
+import {getDisaster, getResources} from './resources.js';
+
+export {disasterDocumentReference, getFirestoreRoot, readDisasterDocument};
+
+/**
+ * Returns the root of the Firestore database. Just firebase.firestore() in
+ * production, but a subcollection in tests to avoid data collisions.
+ * @return {firebase.firestore.Firestore|firebase.firestore.CollectionReference}
+ */
+function getFirestoreRoot() {
+  return inProduction() ? firebase.firestore() :
+                          firebase.firestore().doc('test/' + getTestCookie());
+}
+
+/**
+ * Returns the reference to the disaster metadata document, intended only for
+ * writers.
+ * @return {firebase.firestore.DocumentReference}
+ */
+function disasterDocumentReference() {
+  return getFirestoreRoot()
+      .collection('disaster-metadata')
+      .doc(getResources().year + '-' + getDisaster());
+}
+
+/**
+ * Fetches the document with all metadata for the current disaster. Should only
+ * be called once to avoid excessive fetches.
+ * @return {Promise<firebase.firestore.DocumentSnapshot>}
+ */
+function readDisasterDocument() {
+  return disasterDocumentReference().get();
+}
