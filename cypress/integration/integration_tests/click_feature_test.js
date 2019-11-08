d22c674e08d8c3cbc3928dc8e2b0b697c601a765
Janak Ramakrishnan <janakr@janakr-macbookpro2.roam.corp.google.com>
2019-11-07 19:18:29 -0500

    click-fix

--- cypress/integration/integration_tests/click_feature_test.js
+++ cypress/integration/integration_tests/click_feature_test.js
@@ -82,7 +82,10 @@ function clickAndVerifyBlockGroup() {
   cy.get('[placeholder="Search"]').clear().type('Aldine Estates{enter}');
 
   zoomOut(3);
+  // These waits are embarrassing, but otherwise Travis fails flakily.
+  cy.wait(2000);
   cy.get('.map').click(400, 600);
+  cy.wait(2000);
   cy.get('.map').should('contain', 'SCORE: 72');
   cy.get('.google-visualization-table-tr-sel')
       .find('[class="google-visualization-table-td"]')
