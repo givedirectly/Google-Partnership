717b3fb1afea8276c785574076300a774db6dc17
Janak Ramakrishnan <janakr@janakr-macbookpro2.roam.corp.google.com>
2019-11-11 15:09:17 -0500

    delete-all-data

--- cypress/support/script_loader.js
+++ cypress/support/script_loader.js
@@ -7,6 +7,8 @@ global.host = 'http://localhost:8080/';
 
 let testCookieValue = null;
 
+beforeEach(() => cy.task('logg', 'in before each'));
+
 const scriptMap = new Map([
   [
     'maps',
@@ -111,17 +113,17 @@ const testPrefix = new Date().getTime() + '-';
  * the appropriate cookie.
  */
 function addFirebaseHooks() {
-  before(() => cy.task('initializeTestFirebase', null, {
-                   timeout: 10000,
-                 }).then((token) => global.firestoreCustomToken = token));
+  // before(() => cy.task('initializeTestFirebase', null, {
+  //                  timeout: 10000,
+  //                }).then((token) => global.firestoreCustomToken = token));
   beforeEach(() => {
     testCookieValue = testPrefix + Math.random();
     cy.task('logg', 'Our spec is ' + Cypress.spec.relative);
     cy.log('Our spec is', Cypress.spec.relative);
-    cy.task(
-        'clearAndPopulateTestFirestoreData', testCookieValue, {timeout: 15000});
+    // cy.task(
+    //     'clearAndPopulateTestFirestoreData', testCookieValue, {timeout: 15000});
   });
-  afterEach(() => cy.task('deleteTestData', testCookieValue));
+  // afterEach(() => cy.task('deleteTestData', testCookieValue));
 }
 
 /**
@@ -137,15 +139,15 @@ function doServerEeSetup() {
 if (Cypress.spec.relative.startsWith('cypress/integration/integration_tests')) {
   // Firebase hooks populate/clear test Firebase data.
   addFirebaseHooks();
-  // EE authentication.
-  before(doServerEeSetup);
-  beforeEach(() => {
-    /** wide enough for sidebar */
-    cy.viewport(1100, 1700);
-    cy.setCookie(cypressTestCookieName, testCookieValue);
-    cy.setCookie(firebaseTestTokenCookieName, firestoreCustomToken);
-    cy.setCookie(earthEngineTestTokenCookieName, earthEngineCustomToken);
-  });
+//   // EE authentication.
+//   before(doServerEeSetup);
+//   beforeEach(() => {
+//     /** wide enough for sidebar */
+//     cy.viewport(1100, 1700);
+//     cy.setCookie(cypressTestCookieName, testCookieValue);
+// //    cy.setCookie(firebaseTestTokenCookieName, firestoreCustomToken);
+//     cy.setCookie(earthEngineTestTokenCookieName, earthEngineCustomToken);
+//   });
 }
 
 /**
