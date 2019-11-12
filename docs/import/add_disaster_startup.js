b20abb0b7ba50a6a3bfa18d52f5474899cc37d44
juliexxia <juliexxia@google.com>
2019-11-12 17:12:55 -0500

    format

--- docs/import/add_disaster_startup.js
+++ docs/import/add_disaster_startup.js
@@ -1,7 +1,8 @@
-import SettablePromise from '../settable_promise.js';
 import {authenticateToFirebase, Authenticator} from '../authenticate.js';
-import TaskAccumulator from './task_accumulator.js';
+import SettablePromise from '../settable_promise.js';
+
 import {enableWhenReady} from './add_disaster.js';
+import TaskAccumulator from './task_accumulator.js';
 
 export {taskAccumulator};
 
