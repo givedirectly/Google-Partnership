9d9da53ef941b3b44de77c09e4c23e22ecee7de9
Janak Ramakrishnan <janakr@janakr-macbookpro2.roam.corp.google.com>
2019-11-11 19:29:32 -0500

    click-feature-zoom-off

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -9,10 +9,9 @@ afterEach(function() {
   cy.task(
       'logg',
       // eslint-disable-next-line no-invalid-this
-  'What about ' + this.currentTest.state + ', ' + Cypress.env('TRAVIS'));
+      'What about ' + this.currentTest.state + ', ' + Cypress.env('TRAVIS'));
   // eslint-disable-next-line no-invalid-this
   if (this.currentTest.state === 'failed' && Cypress.env('TRAVIS')) {
     cy.screenshot({capture: 'viewport'});
   }
 });
-
