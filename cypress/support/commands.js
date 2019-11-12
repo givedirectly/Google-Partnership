3c360bfa0a7fe339ecb1522a4e76406ca16f9bcd
Janak Ramakrishnan <janakr@google.com>
2019-11-12 13:19:31 -0500

    click-feature-zoom-off

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
-const LOADING_TIMEOUT = 300000;
+const LOADING_TIMEOUT = 20000;
 
 /**
  * Awaits loading. If no divId is provided, then a full page load is awaited.
@@ -6,21 +6,19 @@ const LOADING_TIMEOUT = 300000;
  * @param {Object} cy The Cypress object.
  * @param {string[]} divIds The ids of specific divs to await loading on.
  */
-Cypress.Commands.add('awaitLoad', (divIds) => {
+Cypress.Commands.add("awaitLoad", (divIds) => {
   let loaderDivs = ['#mapContainer-loader', '#tableContainer-loader'];
   if (divIds) loaderDivs = divIds.map((divId) => '#' + divId + '-loader');
 
   // Ensure overlays are added.
   loaderDivs.forEach((loaderDivId) => {
     cy.get(loaderDivId, {timeout: LOADING_TIMEOUT})
-        .should('have.css', 'opacity')
-        .and('eq', '1');
+        .should('have.css', 'opacity').and('eq', '1');
   });
 
   // Ensure overlays are cleared.
   loaderDivs.forEach((loaderDivId) => {
     cy.get(loaderDivId, {timeout: LOADING_TIMEOUT})
-        .should('have.css', 'opacity')
-        .and('eq', '0');
+        .should('have.css', 'opacity').and('eq', '0');
   });
 });
