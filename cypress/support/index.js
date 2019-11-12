c9bfba626b92027ac7daaefb3005d48c7a0f5c19
Janak Ramakrishnan <janakr@janakr-macbookpro2.roam.corp.google.com>
2019-11-11 19:19:05 -0500

    click-feature-zoom-off

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,4 +5,8 @@ global.tableClass = '.google-visualization-table-table';
 
 // On Travis, the "runner" screenshot only has the error message, not the page.
 // Grab a screenshot of the page as well.
-Cypress.on('fail', (error) => cy.screenshot({capture: 'viewport'}).then(() => throw error));
+afterEach(function () {
+  if (this.currentTest.state === 'failed' && Cypress.env('TRAVIS')) {
+    cy.screenshot({capture: "viewport"});
+  }
+});
