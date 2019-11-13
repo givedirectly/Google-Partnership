3d4c0036f8201fc783ad06ea94ab1fc83c4ba50b
Janak Ramakrishnan <janakr@google.com>
2019-11-13 12:16:40 -0500

    wait-button

--- inner_run.sh
+++ inner_run.sh
@@ -1,1 +1,1 @@
-yarn run cypress run --spec 'cypress/integration/unit_tests/*.js' --env ON_TRAVIS=1 --record --browser chromium --config video=false
+yarn run cypress run --env ON_TRAVIS=1 --record --browser chromium --config video=false
