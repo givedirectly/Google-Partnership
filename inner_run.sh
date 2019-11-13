16eb8adff9c7f9967339eadaa848a48421fab08a
Janak Ramakrishnan <janakr@google.com>
2019-11-13 11:04:18 -0500

    wait-button

---
+++ inner_run.sh
@@ -0,0 +1,1 @@
+yarn run cypress run --spec 'cypress/integration/unit_tests/polygon_draw_test.js' --env ON_TRAVIS=1 --record --config video=false
--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ while true; do
   if git pull | grep 'Already up to date'; then
     sleep 5
   else
-    yarn run cypress run --spec 'cypress/integration/unit_tests/polygon_draw_test.js' --env ON_TRAVIS=1 --record
+    ./inner_run.sh
   fi
 done
