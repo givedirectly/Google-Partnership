4727a95dd56d526808cc03f03f58f1140530f630
Julie <30670028+juliexxia@users.noreply.github.com>
2019-11-15 13:56:37 -0500

    Add delete disaster functionality (#211)

--- cypress/integration/unit_tests/add_disaster_test.js
+++ cypress/integration/unit_tests/add_disaster_test.js
@@ -1,6 +1,6 @@
 import {gdEeStatePrefix, legacyStateDir, legacyStatePrefix} from '../../../docs/ee_paths.js';
 import {getFirestoreRoot} from '../../../docs/firestore_document.js';
-import {addDisaster, createAssetPickers, createOptionFrom, disasters, emptyCallback, getAssetsFromEe, stateAssets, writeNewDisaster} from '../../../docs/import/add_disaster.js';
+import {addDisaster, createAssetPickers, createOptionFrom, deleteDisaster, disasters, emptyCallback, getAssetsFromEe, stateAssets, writeNewDisaster} from '../../../docs/import/add_disaster.js';
 import {addFirebaseHooks, loadScriptsBeforeForUnitTests} from '../../support/script_loader.js';
 
 const KNOWN_STATE = 'WF';
@@ -183,6 +183,30 @@ describe('Unit tests for add_disaster page', () => {
           return addDisaster();
         })
         .then((success) => expect(success).to.be.true);
+  });
+
+  it('deletes a disaster', () => {
+    cy.on('window:confirm', () => true);
+
+    const id = '2002-winter';
+    const states = ['DN, WF'];
+
+    cy.wrap(writeNewDisaster(id, states))
+        .then(
+            () => getFirestoreRoot()
+                      .collection('disaster-metadata')
+                      .doc(id)
+                      .get())
+        .then((doc) => {
+          expect(doc.exists).to.be.true;
+          return cy.window().then((win) => deleteDisaster(win));
+        })
+        .then(
+            () => getFirestoreRoot()
+                      .collection('disaster-metadata')
+                      .doc(id)
+                      .get())
+        .then((doc) => expect(doc.exists).to.be.false);
   });
 });
 
