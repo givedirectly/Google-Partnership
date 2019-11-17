ea10a5f76977820178e7fea676c5e74f2732ac52
Janak Ramakrishnan <janakr@google.com>
2019-11-17 11:49:19 -0500

    retry-tests

--- inner_run.sh
+++
@@ -1,1 +0,0 @@
-yarn run cypress-run --env ON_TRAVIS=1 --record --browser=chromium --config video=false --spec 'cypress/integration/unit_tests/polygon_draw_test.js,cypress/integration/unit_tests/add_disaster_test.js'
