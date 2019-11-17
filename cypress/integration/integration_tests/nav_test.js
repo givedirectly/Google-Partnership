46ad1fb3aa60cf441e9a46dfbffa7450416b120b
ramv13 <ramsai.vellanki@gmail.com>
2019-11-17 18:22:46 -0500

    Fixing test and adding comment for why the click is forced

--- cypress/integration/integration_tests/nav_test.js
+++ cypress/integration/integration_tests/nav_test.js
@@ -1,12 +1,13 @@
 describe('Integration test for the navbar', () => {
   it('Open the nav menu and change pages', () => {
-    cy.visit(host);
+    cy.visit('');
 
     // Click the burger.
     cy.get('#nav-input').click();
 
-    // Click the add asset page link.
-    cy.get('#nav-menu a:first').next().click({force: true});
+    // Click the add asset page link. This is forced because Cypress mistakenly
+    // believes the element is not visible.
+    cy.get('.nav-menu a:first').next().click({force: true});
 
     // The page should now be navigated to add_asset.html.
     cy.url().should('include', '/add_asset.html');
