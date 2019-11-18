6e370a94ee0e3f8ce9845ff463fb6644b28bdcff
ramv13 <ramsai.vellanki@gmail.com>
2019-11-17 23:50:47 -0500

    Resolving comments

--- cypress/integration/integration_tests/nav_test.js
+++ cypress/integration/integration_tests/nav_test.js
@@ -6,7 +6,8 @@ describe('Integration test for the navbar', () => {
     cy.get('#nav-input').click();
 
     // Click the add disaster page link. This is forced because Cypress
-    // mistakenly believes the element is not visible.
+    // mistakenly believes the element is not visible due to the <a> tag being
+    // 0x0.
     cy.get('.nav-menu a:first').next().click({force: true});
 
     // The page should now be navigated to add_disaster.html.
