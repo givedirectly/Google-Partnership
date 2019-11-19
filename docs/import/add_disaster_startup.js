6a64b8d91d1724198863a10a7e3749256d831d05
Janak Ramakrishnan <janakr@google.com>
2019-11-18 19:08:21 -0500

    fix-disaster

--- docs/import/add_disaster_startup.js
+++ docs/import/add_disaster_startup.js
@@ -1,8 +1,9 @@
 import {authenticateToFirebase, Authenticator} from '../authenticate.js';
+import {loadNavbar} from '../navbar.js';
 import SettablePromise from '../settable_promise.js';
+
 import {enableWhenReady, toggleState} from './add_disaster.js';
 import TaskAccumulator from './task_accumulator.js';
-import {loadNavbar} from "../navbar";
 
 export {taskAccumulator};
 
