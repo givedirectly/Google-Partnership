5f41529bacafbb2eb4aaa02f678bcfcb5eeca692
juliexxia <juliexxia@google.com>
2019-11-22 14:25:02 -0500

    format

--- docs/import/add_disaster.js
+++ docs/import/add_disaster.js
@@ -163,7 +163,7 @@ function updateAfterSort(ui) {
 
 /**
  * Look up the real (not table) index of the given row.
- * @param row
+ * @param {JQuery<HTMLTableDataCellElement>} row 
  * @return {string}
  */
 function getRowIndex(row) {
