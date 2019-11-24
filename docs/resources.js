b7a332709dd8fab8648c499a7a07d828c2733177
Janak Ramakrishnan <janakr@google.com>
2019-11-24 14:09:00 -0500

    import_data

--- docs/resources.js
+++ docs/resources.js
@@ -15,9 +15,10 @@ function getDisaster() {
 
 /** @return {string} EE asset path for score asset for the current disaster */
 function getScoreAsset() {
-  // TODO(janakr): When import script finishes, change this to new path, and
-  //  audit code for places that might assume damage is not always 0.
-  return gdEePathPrefix + getDisaster() + '/data-ms-as-nod';
+  // TODO(janakr): Modify map to handle no-damage scenario more intuitively.
+  //  Should make damage threshold, weight 0, probably hide those toggles
+  //  completely. Other changes?
+  return gdEePathPrefix + getDisaster() + '/data-ms-as-tot';
 }
 
 /** The default disaster. */
