fa19b2b9e9e2243cf5e90f29284976bd4527bf33
Janak Ramakrishnan <janakr@google.com>
2019-12-01 11:54:34 -0500

    create-disaster

---
+++ docs/import/create_disaster_lib.js
@@ -0,0 +1,23 @@
+const assetDataTemplate = {
+  asset_data: {
+    damage_asset_path: null,
+    block_group_asset_paths: {},
+    snap_data: {
+      paths: {},
+      snap_key: 'HD01_VD02',
+      total_key: 'HD01_VD01',
+    },
+    svi_asset_paths: {},
+    svi_key: 'RPL_THEMES',
+    income_asset_paths: {
+      NY: incomeData,
+    },
+    income_key: 'testIncomeKey',
+    building_asset_paths: {
+      NY: buildingsCollection,
+    },
+  },
+};
+function createDisasterData(states) {
+
+}
