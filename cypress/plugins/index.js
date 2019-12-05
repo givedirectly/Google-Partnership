0daa793458e81206c031d95605bfcf98038a64ec
Janak Ramakrishnan <janakr@google.com>
2019-12-05 14:23:22 -0500

    add-polygon-coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -120,11 +120,7 @@ module.exports = (on, config) => {
               .then((token) => testApp.auth().signInWithCustomToken(token));
       const deletePromise = deleteTestData(currentTestRoot, testAdminApp);
       const writePromises = [];
-      for (const disaster
-               of ['2017-harvey',
-                   // TODO(janakr): add back so we have multiple disasters.
-                   // '2018-michael',
-      ]) {
+      for (const disaster of ['2017-harvey', '2018-michael']) {
         const documentPath = 'disaster-metadata/' + disaster;
         const prodDisasterDoc = prodApp.firestore().doc(documentPath);
         const testDisasterDocReference = testApp.firestore().doc(
