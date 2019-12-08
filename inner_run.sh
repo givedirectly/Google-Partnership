a1cdfe5926c8e337bd3f1f100d59bbedd64a893f
Janak Ramakrishnan <janakr@google.com>
2019-12-08 15:36:38 -0500

    polygon-real

--- inner_run.sh
+++ inner_run.sh
@@ -1,1 +1,1 @@
-yarn run cypress run --env ON_TRAVIS=1 --browser=chromium --config video=false --spec cypress/integration/unit_tests/polygon_draw_test.js
+yarn run cypress run --env ON_TRAVIS=1 --browser=electron --config video=false --spec cypress/integration/unit_tests/polygon_draw_test.js
