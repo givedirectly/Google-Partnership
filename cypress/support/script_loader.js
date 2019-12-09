53a08b3ccd792c5f3aa387473ac7baaae1d07483
Janak Ramakrishnan <janakr@google.com>
2019-12-08 20:17:50 -0500

    fast-firestore

--- cypress/support/script_loader.js
+++ cypress/support/script_loader.js
@@ -231,6 +231,7 @@ function waitForCallback(callback) {
 function getTimestampRoundedToDays() {
   const date = new Date();
   date.setHours(0);
+  date.setMinutes(0);
   date.setSeconds(0);
   date.setMilliseconds(0);
   return date.getTime();
