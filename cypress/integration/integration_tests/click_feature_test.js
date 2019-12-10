9ee9ea4e0771d4bdcf8ec871dca760eb8ce2b4db
juliexxia <juliexxia@google.com>
2019-12-10 10:48:56 -0500

    remove score verification from flaky click_feature test

--- cypress/integration/integration_tests/click_feature_test.js
+++ cypress/integration/integration_tests/click_feature_test.js
@@ -9,7 +9,7 @@ describe('Integration test for clicking feature', () => {
     cy.visit('');
     cy.awaitLoad();
 
-    clickAndVerifyBlockGroup();
+    clickBlockGroup();
     cy.get('#sidebar-toggle-thresholds').click();
     cy.get('[id="damage threshold"]').clear().type('0.7');
     cy.get('[id="update"]').click();
@@ -22,16 +22,8 @@ describe('Integration test for clicking feature', () => {
 });
 
 /** Convenience function for clicking on the block group we use for testing. */
-function clickAndVerifyBlockGroup() {
+function clickBlockGroup() {
   cy.get('[placeholder="Search"]').clear().type('Greater Greenspoint{enter}');
-
-  cy.wait(6000);
+  cy.wait(4000);
   cy.get('.map').click(900, 500);
-  cy.wait(1000);
-  cy.get('.map').should('contain', 'SCORE: 72');
-  cy.get('.google-visualization-table-tr-sel')
-      .find('[class="google-visualization-table-td"]')
-      .should(
-          'have.text',
-          'Block Group 4, Census Tract 2415, Harris County, Texas21681');
 }
