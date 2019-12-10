cb99aa20f074af3366e44ccb4f293d5b47087336
Janak Ramakrishnan <janakr@google.com>
2019-12-10 17:21:00 -0500

    snackbar-and-disaster-picker

--- docs/toast.js
+++ docs/toast.js
@@ -5,6 +5,12 @@ const TOAST_DURATION_MS = 2000;
 // Track number of messages shown so we don't accidentally hide a later message.
 let showNumber = 0;
 
+/**
+ * Shows a message to the user in a "toast" popup.
+ * @param {string} message Message to show
+ * @param {number} duration Number of milliseconds to show the message. If
+ *     non-positive, message stays up until this method is called again.
+ */
 function showToastMessage(message, duration = TOAST_DURATION_MS) {
   const toast = document.getElementById('toast-message');
   toast.className = 'show';
