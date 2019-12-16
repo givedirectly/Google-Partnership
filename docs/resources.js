c925bc4e758fcad149a1ddc65d6e4c3800b4b8e8
Janak Ramakrishnan <janakr@google.com>
2019-12-16 12:18:57 -0500

    score-asset

--- docs/resources.js
+++ docs/resources.js
@@ -15,11 +15,9 @@ function getDisaster() {
 
 /** @return {string} EE asset path for score asset for the current disaster */
 function getScoreAsset() {
-  // TODO(janakr): Modify map to handle no-damage scenario more intuitively.
-  //  Should make damage threshold, weight 0, probably hide those toggles
-  //  completely. Other changes?
   return gdEePathPrefix + getDisaster() + '/data-ms-as-tot';
 }
+
 
 /** The default disaster. */
 const defaultDisaster = '2017-harvey';
