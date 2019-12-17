40ba00872cfea6aaf18effd1f6e0f7190b145da5
Janak Ramakrishnan <janakr@google.com>
2019-12-17 00:10:12 -0500

    score-asset

--- docs/resources.js
+++ docs/resources.js
@@ -14,12 +14,27 @@ function getDisaster() {
   return defaultDisaster;
 }
 
-/** @return {string} EE asset path for score asset for the current disaster */
+/**
+ * EarthEngine path to score asset.
+ *
+ * This asset may be absent while it is being created. If a previous version of
+ * the asset was created, it will be present under {@link getBackupScoreAsset}.
+ * Therefore, this function should only be called when creating the score asset
+ * and inside run.js when constructing the score asset promise. Naively calling
+ * it could result in errors during the time when the score asset is being
+ * updated.
+ * @return {string}
+ */
 function getScoreAsset() {
+  // TOOD(janakr): rename this path.
   return gdEePathPrefix + getDisaster() + '/data-ms-as-tot';
 }
 
-/** @return {string} EE asset path for previously created score asset */
+/**
+ * EarthEngine path to most recently created score asset before current one.
+ * Not needed unless there is no asset at the path of {@link getScoreAsset}.
+ * @return {string}
+ */
 function getBackupScoreAsset() {
   return gdEePathPrefix + getDisaster() + '/score-asset-previous-version';
 }
