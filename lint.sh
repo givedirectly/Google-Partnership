8df6b3b67b4588236698ccd34b7c6f294e258b7d
Janak Ramakrishnan <janakdr@users.noreply.github.com>
2019-12-18 15:25:25 -0600

    Delete some currently unused files, improve lint.sh (#342)
    These files are no longer being used. We can resurrect them in the future if need be.
    
    Also allow lint.sh to auto-fix the clang-format errors. It just re-invokes itself after that without --fix so that if clang-format somehow fails, it won't infinite loop.

--- lint.sh
+++ lint.sh
@@ -3,21 +3,29 @@ set -e
 # for style failures.
 # TODO(janakr): add configuration to disable individual checks.
 
+readonly first_arg="$1"
 # --diff-filter=dr excludes deleted and renamed files, which don't exist
 # locally anymore.
 # TODO(janakr): this is vulnerable to filenames with spaces.
-modified_js_files="$(git diff --diff-filter=d --name-only \
+readonly modified_js_files="$(git diff --diff-filter=d --name-only \
     master 'docs/*.js' 'docs/import/*.js' 'cypress/integration/**/*.js' \
       'cypress/support/*.js' 'cypress/plugins/*.js')"
 if [[ "$modified_js_files" ]]; then
   if clang-format --style=Google -output-replacements-xml $modified_js_files \
       | grep -c '<replacement ' >/dev/null; then
-    echo 'Found badly formatted files. Run:'
     declare -a badfiles
     for eachfile in $modified_js_files; do
       clang-format --style=Google -output-replacements-xml $eachfile | grep -c '<replacement ' > /dev/null && badfiles+=($eachfile)
     done
-    echo "clang-format -i --style=Google ${badfiles[@]}"
+    if [[ "$first_arg" == "--fix" ]]; then
+      echo "Auto-formatting ${badfiles[@]}"
+      clang-format -i --style=Google ${badfiles[@]}
+      # Skips rest of this script.
+      exec ./lint.sh
+    else
+      echo 'Found badly formatted files. Run:'
+      echo "clang-format -i --style=Google ${badfiles[@]}"
+    fi
     exit 2
   fi
   yarn run eslint $modified_js_files || exit 2
