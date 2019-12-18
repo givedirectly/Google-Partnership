f875d411a1e48773e1bee2bed8c1c2a49f64abce
Janak Ramakrishnan <janakr@google.com>
2019-12-18 16:04:17 -0500

    cleanup

--- lint.sh
+++ lint.sh
@@ -3,6 +3,7 @@ set -e
 # for style failures.
 # TODO(janakr): add configuration to disable individual checks.
 
+first_arg="$1"
 # --diff-filter=dr excludes deleted and renamed files, which don't exist
 # locally anymore.
 # TODO(janakr): this is vulnerable to filenames with spaces.
@@ -12,12 +13,18 @@ modified_js_files="$(git diff --diff-filter=d --name-only \
 if [[ "$modified_js_files" ]]; then
   if clang-format --style=Google -output-replacements-xml $modified_js_files \
       | grep -c '<replacement ' >/dev/null; then
-    echo 'Found badly formatted files. Run:'
     declare -a badfiles
     for eachfile in $modified_js_files; do
       clang-format --style=Google -output-replacements-xml $eachfile | grep -c '<replacement ' > /dev/null && badfiles+=($eachfile)
     done
-    echo "clang-format -i --style=Google ${badfiles[@]}"
+    if [[ "$first_arg" == "--fix" ]]; then
+      echo "Auto-formatting ${badfiles[@]}"
+      clang-format -i --style=Google ${badfiles[@]}
+      ./lint.sh
+    else
+      echo 'Found badly formatted files. Run:'
+      echo "clang-format -i --style=Google ${badfiles[@]}"
+    fi
     exit 2
   fi
   yarn run eslint $modified_js_files || exit 2
