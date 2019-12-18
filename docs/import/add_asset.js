344c4569b224227b52c87dd321437c16ff4c2514
Janak Ramakrishnan <janakr@google.com>
2019-12-18 16:07:43 -0500

    redirect-authenticate

--- docs/import/add_asset.js
+++ docs/import/add_asset.js
@@ -47,11 +47,10 @@ ee.data.setCloudApiEnabled(true);
 // 3 tasks: EE authentication, OAuth2 token retrieval, and page load.
 const taskAccumulator = new TaskAccumulator(3, enableWhenReady);
 // Perform EE login/Google OAuth2 process.
-// TODO(janakr): fix this page or delete.
-// const authenticator = new Authenticator(
-//     setUpAllHeaders, () => taskAccumulator.taskCompleted(), setStatusDiv,
-//     [storageScope]);
-// authenticator.start();
+const authenticator = new Authenticator(
+    setUpAllHeaders, () => taskAccumulator.taskCompleted(), setStatusDiv,
+    [storageScope]);
+authenticator.start();
 
 /**
  * Processes files and asset name user gave, mostly asynchronously.
