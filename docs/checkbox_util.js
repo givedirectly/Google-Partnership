8d0aefe61279e2506cbb39970fbd55be3b435ac1
Janak Ramakrishnan <janakr@google.com>
2019-12-23 21:13:29 -0500

    layer-fail

---
+++ docs/checkbox_util.js
@@ -0,0 +1,20 @@
+export {getCheckBoxId, getCheckBoxRowId};
+
+/**
+ * Creates the id of a show/hide checkbox.
+ *
+ * @param {number|string} index Number unless 'score'
+ * @return {string}
+ */
+function getCheckBoxId(index) {
+  return 'layer-' + index + '-checkbox';
+}
+
+/**
+ * Creates the id of a show/hide checkbox's row.
+ * @param {number|string} index See {@link getCheckBoxId}
+ * @return {string}
+ */
+function getCheckBoxRowId(index) {
+  return getCheckBoxId(index) + '-row';
+}
