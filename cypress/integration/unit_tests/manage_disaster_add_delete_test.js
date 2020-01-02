givedirectly/Google-Partnership

commit dc01eb276964b4289b995a08a52d0df3b202c916

Handle EE folder creation errors and Firestore errors in disaster creation (#361)
We just show a snackbar error. Not sure there's more we can do, because it's unexpected.

This will slightly slow down creation, because it's EE then Firestore, but worth it.

Also fix slightly broken test: under the covers we were creating states for the letters in 'IG, MY' because inputs can't have array values.

Closes #331

docs/import/manage_disaster.js
@@ -1,4 +1,5 @@
 import {eeLegacyPathPrefix, legacyStateDir} from '../ee_paths.js';
+import {showError} from '../error.js';
 import {disasterCollectionReference} from '../firestore_document.js';
 import {convertEeObjectToPromise, latLngToGeoPoint, transformGeoPointArrayToLatLng} from '../map_util.js';
 import {getDisaster} from '../resources.js';
@@ -367,21 +368,18 @@ function addDisaster() {
 
 /**
  * Writes the given details to a new disaster entry in firestore. Fails if
- * there is an existing disaster with the same details.
+ * there is an existing disaster with the same details or there are errors
+ * writing to EarthEngine or Firestore. Tells the user in all failure cases.
  *
- * TODO(janakr): If the user starts editing a disaster before the Firestore
- *  write completes, their edit could be overwritten by the initial Firestore
- *  write here. Probably solved similar to the delete disaster issue: don't
- *  actually show the disaster as editable until this write completes.
  * @param {string} disasterId of the form <year>-<name>
  * @param {Array<string>} states array of state (abbreviations)
- * @return {Promise<boolean>} returns true after successful write to firestore
- * and earth engine folder creations.
+ * @return {Promise<boolean>} Returns true if EarthEngine folders created
+ *     successfully and Firestore write was successful
  */
-function writeNewDisaster(disasterId, states) {
+async function writeNewDisaster(disasterId, states) {
   if (disasterData.has(disasterId)) {
     setStatus('Error: disaster with that name and year already exists.');
-    return Promise.resolve(false);
+    return false;
   }
   clearStatus();
   const currentData = createDisasterData(states);
@@ -389,51 +387,50 @@ function writeNewDisaster(disasterId, states) {
   // We know there are no assets in folder yet.
   disasterAssets.set(disasterId, Promise.resolve(new Map()));
 
-  const folderCreationPromises = [];
-  folderCreationPromises.push(
-      getCreateFolderPromise(eeLegacyPathPrefix + disasterId));
-  for (const state of states) {
-    folderCreationPromises.push(
-        getCreateFolderPromise(legacyStateDir + '/' + state));
+  const eeFolderPromises =
+      [getCreateFolderPromise(eeLegacyPathPrefix + disasterId)];
+  states.forEach(
+      (state) => eeFolderPromises.push(
+          getCreateFolderPromise(legacyStateDir + '/' + state)));
+
+  const tailError = '" You can try refreshing the page';
+  // Wait on EE folder creation to do the Firestore write, since if folder
+  // creation fails we don't want to have to undo the write.
+  try {
+    await Promise.all(eeFolderPromises);
+  } catch (err) {
+    showError('Error creating EarthEngine folders: "' + err + tailError);
+    return false;
   }
-
-  const eePromisesResult = Promise.all(folderCreationPromises).then(() => {
-    const disasterPicker = $('#disaster-dropdown');
-    const disasterOptions = disasterPicker.children();
-    let added = false;
-    // We expect this recently created disaster to go near the top of the list,
-    // so do a linear scan down. Note: let's hope this tool isn't being used in
-    // the year 10000.
-    // Comment needed to quiet eslint.
-    disasterOptions.each(/* @this HTMLElement */ function() {
-      if ($(this).val() < disasterId) {
-        $(createOptionFrom(disasterId)).insertBefore($(this));
-        added = true;
-        return false;
-      }
-    });
-    if (!added) disasterPicker.append(createOptionFrom(disasterId));
-    toggleState(true);
-
-    disasterPicker.val(disasterId).trigger('change');
+  try {
+    await disasterCollectionReference().doc(disasterId).set(currentData);
+  } catch (err) {
+    const message = err.message ? err.message : err;
+    showError('Error writing to Firestore: "' + message + tailError);
+    return false;
+  }
+  const disasterPicker = $('#disaster-dropdown');
+  let added = false;
+  // We expect this recently created disaster to go near the top of the list, so
+  // do a linear scan down.
+  // Note: let's hope this tool isn't being used in the year 10000.
+  // Comment needed to quiet eslint.
+  disasterPicker.children().each(/* @this HTMLElement */ function() {
+    if ($(this).val() < disasterId) {
+      $(createOptionFrom(disasterId)).insertBefore($(this));
+      added = true;
+      return false;
+    }
   });
+  if (!added) disasterPicker.append(createOptionFrom(disasterId));
+  toggleState(true);
 
-  return Promise
-      .all([
-        eePromisesResult,
-        disasterCollectionReference().doc(disasterId).set(currentData),
-      ])
-      .then(() => true);
+  disasterPicker.val(disasterId).trigger('change');
+  return true;
 }
 
 /**
  * Returns a promise that resolves on the creation of the given folder.
- *
- * This will print a console error for anyone other than the gd
- * account. Ee console seems to have the power to grant write access
- * to non-owners but it doesn't seem to work. Sent an email to
- * gestalt.
- * TODO: replace with setIamPolicy when that works.
  * TODO: add status bar for when this is finished.
  *
  * @param {string} dir asset path of folder to create
@@ -442,10 +439,21 @@ function writeNewDisaster(disasterId, states) {
  */
 function getCreateFolderPromise(dir) {
   return new Promise(
-      (resolve) => ee.data.createFolder(
-          dir, false,
-          () => ee.data.setAssetAcl(
-              dir, {all_users_can_read: true}, () => resolve())));
+      (resolve, reject) =>
+          ee.data.createFolder(dir, false, (result, failure) => {
+            if (failure) {
+              reject(failure);
+              return;
+            }
+            ee.data.setAssetAcl(
+                dir, {all_users_can_read: true}, (result, failure) => {
+                  if (failure) {
+                    reject(failure);
+                    return;
+                  }
+                  resolve(result);
+                });
+          }));
 }
 
 /**

cypress/integration/unit_tests/manage_disaster_add_delete_test.js
@@ -1,3 +1,5 @@
+import * as ErrorLib from '../../../docs/error.js';
+import * as FirestoreDocument from '../../../docs/firestore_document.js';
 import {getFirestoreRoot} from '../../../docs/firestore_document.js';
 import {assetDataTemplate} from '../../../docs/import/create_disaster_lib.js';
 import {disasterData} from '../../../docs/import/manage_disaster';
@@ -88,10 +90,16 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
     cy.wrap(writeNewDisaster(id, states))
         .then((success) => {
           expect(success).to.be.true;
+          expect(createFolderStub).to.be.calledTwice;
+          expect(setAclsStub).to.be.calledTwice;
+          createFolderStub.resetHistory();
+          setAclsStub.resetHistory();
           return writeNewDisaster(id, states);
         })
         .then((success) => {
           expect(success).to.be.false;
+          expect(createFolderStub).to.not.be.called;
+          expect(setAclsStub).to.not.be.called;
           const status = $('#compute-status');
           expect(status.is(':visible')).to.be.true;
           expect(status.text())
@@ -103,7 +111,10 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
   it('tries to write a disaster with bad info, then fixes it', () => {
     const year = createAndAppend('input', 'year');
     const name = createAndAppend('input', 'name');
-    const states = createAndAppend('input', 'states');
+    const states = createAndAppend('select', 'states');
+    states.prop('multiple', 'multiple')
+        .append(createSelectedOption('IG'))
+        .append(createSelectedOption('MY'));
     const status = $('#compute-status');
 
     cy.wrap(addDisaster())
@@ -112,10 +123,11 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
           expect(status.is(':visible')).to.be.true;
           expect(status.text())
               .to.eql('Error: Disaster name, year, and states are required.');
+          expect(createFolderStub).to.not.be.called;
+          expect(setAclsStub).to.not.be.called;
 
           year.val('hello');
           name.val('my name is');
-          states.val(['IG', 'MY']);
           return addDisaster();
         })
         .then((success) => {
@@ -125,6 +137,9 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
 
           year.val('2000');
           name.val('HARVEY');
+          expect(createFolderStub).to.not.be.called;
+          expect(setAclsStub).to.not.be.called;
+
           return addDisaster();
         })
         .then((success) => {
@@ -135,10 +150,64 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
                   'Error: disaster name must be comprised of only ' +
                   'lowercase letters');
 
+          expect(createFolderStub).to.not.be.called;
+          expect(setAclsStub).to.not.be.called;
           name.val('harvey');
           return addDisaster();
         })
-        .then((success) => expect(success).to.be.true);
+        .then((success) => {
+          expect(success).to.be.true;
+          expect(createFolderStub).to.be.calledThrice;
+          expect(setAclsStub).to.be.calledThrice;
+        });
+  });
+
+  /**
+   * Creates an option with the given text, already selected.
+   * @param {string} text
+   * @return {JQuery<HTMLOptionElement>}
+   */
+  function createSelectedOption(text) {
+    return $(document.createElement('option')).val(text).prop('selected', true);
+  }
+
+  it('Error creating EE folder', () => {
+    const id = '2005-summer';
+    const states = [KNOWN_STATE];
+    const errorStub = cy.stub(ErrorLib, 'showError');
+    // Restore actual ee.createFolder. Tests don't have permission to create
+    // folders, so this exercises EE failure mode better than we could fake it.
+    createFolderStub.restore();
+    const firestoreStub =
+        cy.stub(FirestoreDocument, 'disasterCollectionReference');
+    cy.wrap(writeNewDisaster(id, states)).then((success) => {
+      expect(success).to.be.false;
+      expect(firestoreStub).to.not.be.called;
+      expect(disasterData).to.be.empty;
+      expect(errorStub).to.be.calledOnce;
+      expect(errorStub).to.be.calledWith(
+          'Error creating EarthEngine folders: "Asset ' +
+          '\'projects/earthengine-legacy/assets/users/gd\' does not exist or ' +
+          'doesn\'t allow this operation." You can try refreshing the page');
+    });
+  });
+
+  it('Error writing to Firestore', () => {
+    const id = '2005-summer';
+    const states = [KNOWN_STATE];
+    const errorStub = cy.stub(ErrorLib, 'showError');
+    // Tests don't have permission to write to root Firestore, so this will
+    // exercise Firestore functionality better than we could fake it.
+    cy.stub(FirestoreDocument, 'disasterCollectionReference')
+        .returns(firebase.firestore().collection('disaster-metadata'));
+    cy.wrap(writeNewDisaster(id, states)).then((success) => {
+      expect(success).to.be.false;
+      expect(disasterData).to.be.empty;
+      expect(errorStub).to.be.calledOnce;
+      expect(errorStub).to.be.calledWith(
+          'Error writing to Firestore: "Missing or insufficient ' +
+          'permissions." You can try refreshing the page');
+    });
   });
 
   it('deletes a disaster', () => {

docs/error.js
@@ -17,10 +17,10 @@ function createError(msg) {
  * Logs an error message to the console and shows a snackbar notification.
  *
  * @param {string} msg the message to be logged
- * @param {string} snackbarMsg the message to be shown in the notification (this
- *     can be omitted if the message should just be the logged message).
+ * @param {?string} snackbarMsg the message to be shown in the notification
+ *     (this can be omitted if the message should just be the logged message).
  */
-function showError(msg, snackbarMsg) {
+function showError(msg, snackbarMsg = null) {
   if (snackbarMsg == null) snackbarMsg = msg;
   console.error(msg);
   showSnackbar(

commit 8ac5c961282a94afc11f9562ed86dc9da8cb9fb6

Fix bug where we errored if folder already existed (#376)
Disasters can share states, and if a disaster folder is created as part of an unsuccessful add (say because Firestore write failed), we want to be able to re-use it.

Also fix bug where we were writing locally before EE and Firestore succeeded, so failures left the page in a bad state. We actually had test coverage for this, but Cypress apparently has a bug.

Main bug here was introduced by #361: we added error handling there, but didn't tolerate "already existing" errors. I had thought in the back of my mind that overwrite=false would prevent errors, but didn't actually test it.

docs/import/manage_disaster.js
@@ -362,11 +362,6 @@ async function writeNewDisaster(disasterId, states) {
     return false;
   }
   clearStatus();
-  const currentData = createDisasterData(states);
-  disasterData.set(disasterId, currentData);
-  // We know there are no assets in folder yet.
-  disasterAssets.set(disasterId, Promise.resolve(new Map()));
-
   const eeFolderPromises =
       [getCreateFolderPromise(eeLegacyPathPrefix + disasterId)];
   states.forEach(
@@ -382,6 +377,8 @@ async function writeNewDisaster(disasterId, states) {
     showError('Error creating EarthEngine folders: "' + err + tailError);
     return false;
   }
+
+  const currentData = createDisasterData(states);
   try {
     await disasterCollectionReference().doc(disasterId).set(currentData);
   } catch (err) {
@@ -389,6 +386,11 @@ async function writeNewDisaster(disasterId, states) {
     showError('Error writing to Firestore: "' + message + tailError);
     return false;
   }
+
+  disasterData.set(disasterId, currentData);
+  // We know there are no assets in folder yet.
+  disasterAssets.set(disasterId, Promise.resolve(new Map()));
+
   const disasterPicker = $('#disaster-dropdown');
   let added = false;
   // We expect this recently created disaster to go near the top of the list, so
@@ -421,7 +423,7 @@ function getCreateFolderPromise(dir) {
   return new Promise(
       (resolve, reject) =>
           ee.data.createFolder(dir, false, (result, failure) => {
-            if (failure) {
+            if (failure && !failure.startsWith('Cannot overwrite asset ')) {
               reject(failure);
               return;
             }

cypress/integration/unit_tests/manage_disaster_add_delete_test.js
@@ -1,3 +1,4 @@
+import {legacyStateDir} from '../../../docs/ee_paths.js';
 import * as ErrorLib from '../../../docs/error.js';
 import * as FirestoreDocument from '../../../docs/firestore_document.js';
 import {getFirestoreRoot} from '../../../docs/firestore_document.js';
@@ -24,9 +25,8 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
   let setAclsStub;
   beforeEach(() => {
     disasterData.clear();
-    createFolderStub =
-        cy.stub(ee.data, 'createFolder')
-            .callsFake((asset, overwrite, callback) => callback());
+    createFolderStub = cy.stub(ee.data, 'createFolder');
+    createFolderStub.callsFake((asset, overwrite, callback) => callback());
     setAclsStub = cy.stub(ee.data, 'setAssetAcl')
                       .callsFake((asset, acls, callback) => callback());
   });
@@ -183,12 +183,29 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
     cy.wrap(writeNewDisaster(id, states)).then((success) => {
       expect(success).to.be.false;
       expect(firestoreStub).to.not.be.called;
-      expect(disasterData).to.be.empty;
+      // https://github.com/cypress-io/cypress/issues/6072 means no ".empty" :(
+      expect(disasterData).to.have.property('size', 0);
       expect(errorStub).to.be.calledOnce;
       expect(errorStub).to.be.calledWith(
           'Error creating EarthEngine folders: "Asset ' +
           '\'projects/earthengine-legacy/assets/users/gd\' does not exist or ' +
           'doesn\'t allow this operation." You can try refreshing the page');
+    });
+  });
+
+  it('Tolerates already existing folder', () => {
+    const id = '2005-summer';
+    const states = [KNOWN_STATE];
+    const errorStub = cy.stub(ErrorLib, 'showError');
+    // Sadly, don't know how to get ee to actually return this error for real.
+    createFolderStub
+        .withArgs(legacyStateDir + KNOWN_STATE, false, Cypress.sinon.match.func)
+        .callsFake((asset, overwrite, callback) => {
+          callback(null, 'Cannot overwrite asset \'' + asset + '\'.');
+        });
+    cy.wrap(writeNewDisaster(id, states)).then((success) => {
+      expect(success).to.be.true;
+      expect(errorStub).to.not.be.called;
     });
   });
 
@@ -202,7 +219,8 @@ describe('Add/delete-related tests for manage_disaster.js', () => {
         .returns(firebase.firestore().collection('disaster-metadata'));
     cy.wrap(writeNewDisaster(id, states)).then((success) => {
       expect(success).to.be.false;
-      expect(disasterData).to.be.empty;
+      // https://github.com/cypress-io/cypress/issues/6072 means no ".empty" :(
+      expect(disasterData).to.have.property('size', 0);
       expect(errorStub).to.be.calledOnce;
       expect(errorStub).to.be.calledWith(
           'Error writing to Firestore: "Missing or insufficient ' +