f5d5cec3b76ba14fcc995107fa144327a81e19d7
juliexxia <juliexxia@google.com>
2020-01-14 19:54:59 -0500

    format

--- docs/firebase_layers.js
+++ docs/firebase_layers.js
@@ -125,7 +125,8 @@ function getLinearGradient(colorFunction) {
   let gradientString = 'linear-gradient(to right';
   switch (colorFunction.currentStyle) {
     case ColorStyle.CONTINUOUS:
-      gradientString += ', rgb(255,255,255), ' + colorToRgbString(colorFunction['color']);
+      gradientString +=
+          ', rgb(255,255,255), ' + colorToRgbString(colorFunction['color']);
       break;
     case ColorStyle.DISCRETE:
       const colors = [...(new Set(Object.values(colorFunction['colors'])))];
