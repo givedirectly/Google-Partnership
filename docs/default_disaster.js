52880be31538c736b9d9059f9e67342b53a9874d
Julie <30670028+juliexxia@users.noreply.github.com>
2020-01-16 14:40:29 -0500

    factor out default disaster to standalone file for easy access (#432)
    Minimal docs

---
+++ docs/default_disaster.js
@@ -0,0 +1,12 @@
+/**
+ * The default disaster that will show up the very first time a user visits
+ * the map. In order to update which disaster users land on the first time,
+ * update the string below and commit to the github repository. The string
+ * should always be in <year>-<name> format (with the name being all lowercase).
+ *
+ * After the first time, we locally store the last disaster they were on
+ * and always reopen that one.
+ */
+const DEFAULT_DISASTER = '2017-harvey';
+
+export {DEFAULT_DISASTER};
