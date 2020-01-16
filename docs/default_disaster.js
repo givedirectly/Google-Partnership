5c4f8216c3ff9ef59eeb850d1e13b31ef1842125
Julie Xia <juliexxia@.google.com>
2020-01-16 14:12:19 -0500

    deflake

--- docs/default_disaster.js
+++ docs/default_disaster.js
@@ -1,6 +1,6 @@
 /**
  * The default disaster that will show up the very first time a user visits
- * the web app. In order to update which disaster users land on the first time,
+ * the map. In order to update which disaster users land on the first time,
  * update the string below and commit to the github repository. The string
  * should always be in <year>-<name> format (with the name being all lowercase).
  *
