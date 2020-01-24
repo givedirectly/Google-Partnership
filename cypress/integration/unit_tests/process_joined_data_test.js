751ecbd9a9c6bd7d1b80214fb44d2424807b4f73
Janak Ramakrishnan <janakr@google.com>
2020-01-24 17:05:56 -0500

    many-expected

--- cypress/integration/unit_tests/process_joined_data_test.js
+++ cypress/integration/unit_tests/process_joined_data_test.js
@@ -158,7 +158,7 @@ describe('Unit test for processed_joined_data.js', () => {
                 })))
         .then(({featuresList, columnsFound}) => {
           expect(columnsFound).to.eql([
-            'GEOID',
+            '___GD_GOOGLE_DELPHI_GEOID',
             'district descript',
             'SCORE',
             'poverty rate',
