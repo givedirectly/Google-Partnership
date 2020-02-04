givedirectly/Google-Partnership

commit 175e194b188948a23d24da50f5207e8a0a980ffb

Move harvey layers into firebase (#186)
This PR reads feature collection asset information out of firestore for rendering as layers on the map.

Structure of asset information in firestore:
In disaster-metadata => <year> => <disaster> => layers we hold a map of <asset path : <data>>

all <data> instances contain the following fields:

asset-type {int}: 0 (Feature), 1 (FeatureCollection), 2 (Image), 3 (ImageCollection)
display-name {string}
display-on-load {boolean}
color-function {Object}
We have three types of coloring schemes: continuous, discrete, and single color. Each has a different schema for the color-fxn object:
continuous:

continuous {boolean}: set to true
base-color {string}: from set of known colors
field {string}: name of property we're using to calculate color
max {number}: max value of field
min {number}: min value of field
opacity {number}
discrete:

continuous {boolean}: set to false
field {string}: name of property we're using to calculate color
colors {Map}: map of field value {string} to color {string} (from known set of colors)
opacity {number}
single-color:

single-color {string}: from set of known colors
opacity {number}
Note - all color-fxn objects have the opacity field.

TODO:

programmatic way to enter asset information into firestore

docs/firebase_layers.js
@@ -1,0 +1,123 @@
+export {
+  colorMap,
+  firebaseLayers,
+  getStyleFunction,
+  initializeFirebaseLayers,
+  LayerType,
+};
+
+// The collection of firebase layers.
+let firebaseLayers;
+
+/**
+ * Initialize the var once we receive the layers from firebase.
+ * @param {Object} layers
+ */
+function initializeFirebaseLayers(layers) {
+  firebaseLayers = layers;
+}
+
+const LayerType = {
+  FEATURE: 0,
+  FEATURE_COLLECTION: 1,
+  IMAGE: 2,
+  IMAGE_COLLECTION: 3,
+};
+Object.freeze(LayerType);
+
+// Map of FeatureCollection ee asset path to style function so we don't need to
+// recreate it every time.
+const styleFunctions = new Map();
+
+/**
+ * Returns style function for this FeatureCollection ee asset.
+ * @param {String} assetName asset path
+ * @return {Function}
+ */
+function getStyleFunction(assetName) {
+  return styleFunctions.has(assetName) ? styleFunctions.get(assetName) :
+                                         createStyleFunction(assetName);
+}
+
+/**
+ * Creates and stores the style function for a FeatureCollection ee asset.
+ * @param {String} assetName asset path
+ * @return {Function}
+ */
+function createStyleFunction(assetName) {
+  const colorFunctionProperties = firebaseLayers[assetName]['color-function'];
+  let styleFunction;
+  if (colorFunctionProperties['single-color']) {
+    const color = colorMap.get(colorFunctionProperties['single-color']);
+    styleFunction = () => color;
+  } else {
+    const continuous = colorFunctionProperties['continuous'];
+    const field = colorFunctionProperties['field'];
+    const opacity = colorFunctionProperties['opacity'];
+    styleFunction = continuous ?
+        createContinuousFunction(
+            field, opacity, colorFunctionProperties['min'],
+            colorFunctionProperties['max'],
+            colorFunctionProperties['base-color']) :
+        createDiscreteFunction(
+            field, opacity, colorFunctionProperties['colors']);
+  }
+  styleFunctions.set(assetName, styleFunction);
+  return styleFunction;
+}
+
+/**
+ * Creates a continuous color function for a feature collection from the given
+ * base color to white.
+ *
+ * @param {String} field property whose value is used to determine color
+ * @param {number} opacity
+ * @param {number} minVal minVal of {@param field}
+ * @param {number} maxVal maxVal of {@param field}
+ * @param {String} color base color
+ * @return {Function}
+ */
+function createContinuousFunction(field, opacity, minVal, maxVal, color) {
+  const colorRgb = colorMap.get(color);
+  const white = colorMap.get('white');
+  return (feature) => {
+    const value = feature['properties'][field];
+    const rgba = [];
+    for (let i = 0; i < 3; i++) {
+      rgba.push(
+          ((colorRgb[i] * (value - minVal)) + (white[i] * (maxVal - value))) /
+          2);
+    }
+    rgba.push(opacity);
+    return rgba;
+  };
+}
+
+/**
+ * Creates a discrete color function for a feature collection.
+ * @param {String} field property whose value is used to determine color
+ * @param {number} opacity
+ * @param {Map<String, String>} colors field value:color (e.g. 'minor-damage':
+ *     'red')
+ * @return {Function}
+ */
+function createDiscreteFunction(field, opacity, colors) {
+  // TODO: allow for a default color if field value color isn't specified.
+  return (feature) => {
+    const color = colors[feature['properties'][field]];
+    const rgba = colorMap.get(color);
+    rgba.push(opacity);
+    return rgba;
+  };
+}
+
+const colorMap = new Map([
+  ['red', [255, 0, 0]],
+  ['orange', [255, 140, 0]],
+  ['yellow', [255, 255, 0]],
+  ['green', [0, 255, 0]],
+  ['blue', [0, 0, 255]],
+  ['purple', [128, 0, 128]],
+  ['black', [0, 0, 0]],
+  ['white', [255, 255, 255]],
+]);

docs/layer_util.js
@@ -1,6 +1,7 @@
 import {mapContainerId} from './dom_constants.js';
-import {assets, EarthEngineAsset} from './earth_engine_asset.js';
+import {terrainStyle} from './earth_engine_asset.js';
 import {createError} from './error.js';
+import {colorMap, firebaseLayers, getStyleFunction, LayerType} from './firebase_layers.js';
 import {addLoadingElement, loadingElementFinished} from './loading.js';
 import {convertEeObjectToPromise} from './map_util.js';
 
@@ -21,11 +22,10 @@ export {layerArray, layerMap, LayerMapValue};
 const scoreLayerName = 'score';
 
 /**
- * Keep a map of asset name -> data, index, display status. Data is lazily
- * generated i.e. pre-known assets that don't display by default will have an
+ * Keep a map of layer name -> data, index, display status. Data is lazily
+ * generated i.e. pre-known layers that don't display by default will have an
  * entry in this map, but the LayerMapValue will have a null data field until we
- * fetch the data when the user wants to display it. Currently assume we're only
- * working with one map.
+ * fetch the data when the user wants to display it.
  */
 const layerMap = new Map();
 
@@ -77,27 +77,27 @@ function setMapToDrawLayersOn(map) {
 /**
  * Toggles on displaying an asset on the map.
  *
- * @param {string} assetName
+ * @param {string} layerName
  * @param {google.maps.Map} map main map
  */
-function toggleLayerOn(assetName, map) {
-  const currentLayerMapValue = layerMap.get(assetName);
+function toggleLayerOn(layerName, map) {
+  const currentLayerMapValue = layerMap.get(layerName);
   currentLayerMapValue.displayed = true;
   if (currentLayerMapValue.data) {
-    addLayerFromFeatures(currentLayerMapValue, assetName);
+    addLayerFromFeatures(currentLayerMapValue, layerName);
   } else {
-    addLayer(assetName, currentLayerMapValue.index, map);
+    addLayer(layerName, currentLayerMapValue.index, map);
   }
 }
 
 /**
  * Toggles off displaying an asset on the map.
  *
- * @param {string} assetName
+ * @param {string} layerName
  * @param {google.maps.Map} map main map
  */
-function toggleLayerOff(assetName, map) {
-  removeLayer(assetName, map);
+function toggleLayerOff(layerName, map) {
+  removeLayer(layerName, map);
 }
 
 /**
@@ -120,26 +120,26 @@ function getColorOfFeature(feature) {
  *
  * @param {google.maps.Map} map
  * @param {ee.Element} layer
- * @param {string} assetName
+ * @param {string} layerName
  * @param {number} index
  */
-function addImageLayer(map, layer, assetName, index) {
-  const imgStyles = assets[assetName].getVisParams();
-  if (assets[assetName].getStylingFunction()) {
-    layer = assets[assetName].getStylingFunction()(layer);
+function addImageLayer(map, layer, layerName, index) {
+  const imgStyles = firebaseLayers[layerName]['vis-params'];
+  if (firebaseLayers[layerName]['use-terrain-style']) {
+    layer = terrainStyle(layer);
   }
   // Add a null-overlay entry to layerMap while waiting for the callback to
   // finish.
-  layerMap[assetName] = new LayerMapValue(null, index, true);
+  layerMap[layerName] = new LayerMapValue(null, index, true);
   layer.getMap({
     visParams: imgStyles,
     callback: (layerId, failure) => {
       if (layerId) {
-        layerMap[assetName].overlay = addLayerFromId(
-            map, assetName, layerId, index, layerMap[assetName].displayed);
+        layerMap[layerName].overlay = addLayerFromId(
+            map, layerName, layerId, index, layerMap[layerName].displayed);
       } else {
         // TODO: if there's an error, disable checkbox, add tests for this.
-        layerMap[assetName].displayed = false;
+        layerMap[layerName].displayed = false;
         createError('getting id')(failure);
       }
     },
@@ -152,13 +152,13 @@ function addImageLayer(map, layer, assetName, index) {
  * callbacks or similar to that overlay.
  *
  * @param {google.maps.Map} map
- * @param {string} assetName
+ * @param {string} layerName
  * @param {Object} layerId
  * @param {number} index
  * @param {boolean} displayed
  * @return {ee.MapLayerOverlay}
  */
-function addLayerFromId(map, assetName, layerId, index, displayed) {
+function addLayerFromId(map, layerName, layerId, index, displayed) {
   const overlay = new ee.MapLayerOverlay(
       'https://earthengine.googleapis.com/map', layerId.mapid, layerId.token,
       {});
@@ -166,9 +166,9 @@ function addLayerFromId(map, assetName, layerId, index, displayed) {
   overlay.addTileCallback((tileEvent) => {
     if (tileEvent.count == 0) {
       loadingElementFinished(mapContainerId);
-      layerMap[assetName].loading = false;
-    } else if (!layerMap[assetName].loading) {
-      layerMap[assetName].loading = true;
+      layerMap[layerName].loading = false;
+    } else if (!layerMap[layerName].loading) {
+      layerMap[layerName].loading = true;
       addLoadingElement(mapContainerId);
     }
   });
@@ -193,29 +193,23 @@ function addLayerFromId(map, assetName, layerId, index, displayed) {
  * special handling, so deck.gl is forced to re-render it on parameter changes.
  *
  * @param {LayerMapValue} layerMapValue
- * @param {string} assetName
+ * @param {string} layerName
  */
-function addLayerFromFeatures(layerMapValue, assetName) {
-  const hasStyleFunction =
-      assets[assetName] && assets[assetName].getStylingFunction();
-  const styleFunction = hasStyleFunction ?
-      assets[assetName].getStylingFunction() :
-      getColorOfFeature;
+function addLayerFromFeatures(layerMapValue, layerName) {
   layerArray[layerMapValue.index] = new deck.GeoJsonLayer({
-    id: assetName,
+    id: layerName,
     data: layerMapValue.data,
     pointRadiusMinPixels: 1,
     getRadius: 10,
     // TODO(janakr): deck.gl docs claim that the "color" property should
     // automatically color the features, but it doesn't appear to work:
     // https://deck.gl/#/documentation/deckgl-api-reference/layers/geojson-layer?section=getelevation-function-number-optional-transition-enabled
-    getFillColor: styleFunction,
+    getFillColor: firebaseLayers[layerName] ? getStyleFunction(layerName) :
+                                              getColorOfFeature,
     visible: layerMapValue.displayed,
   });
   redrawLayers();
 }
-
-const black = [0, 0, 0, 255];
 
 /**
  * Utility function to return the given color if defined, or black if undefined.
@@ -224,7 +218,7 @@ const black = [0, 0, 0, 255];
  * @return {Array} RGBA color specification as an array
  */
 function showColor(color) {
-  return color ? color : black;
+  return color ? color : colorMap.get('black');
 }
 
 // 250M objects in a FeatureCollection ought to be enough for anyone.
@@ -233,24 +227,28 @@ const maxNumFeaturesExpected = 250000000;
 /**
  * Convenience wrapper for addLayerFromGeoJsonPromise.
  *
- * @param {string} assetName Name of EarthEngine FeatureCollection.
+ * @param {string} layerName Name of EarthEngine FeatureCollection.
  * @param {number} index Ordering of layer (higher is more visible).
  * @param {google.maps.Map} map main map
  */
-function addLayer(assetName, index, map) {
-  switch (assets[assetName].getType()) {
-    case EarthEngineAsset.Type.IMAGE:
-      addImageLayer(map, ee.Image(assetName), assetName, index);
+function addLayer(layerName, index, map) {
+  switch (firebaseLayers[layerName]['asset-type']) {
+    case LayerType.IMAGE:
+      addImageLayer(map, ee.Image(layerName), layerName, index);
       break;
-    case EarthEngineAsset.Type.IMAGECOLLECTION:
-      addImageLayer(map, ee.ImageCollection(assetName), assetName, index);
+    case LayerType.IMAGE_COLLECTION:
+      addImageLayer(map, ee.ImageCollection(layerName), layerName, index);
       break;
-    default:
+    case LayerType.FEATURE:
+    case LayerType.FEATURE_COLLECTION:
       addLayerFromGeoJsonPromise(
           convertEeObjectToPromise(
-              ee.FeatureCollection(assetName).toList(maxNumFeaturesExpected)),
-          assetName, index);
+              ee.FeatureCollection(layerName).toList(maxNumFeaturesExpected)),
+          layerName, index);
       break;
+    default:
+      createError('parsing layer type during add')(
+          '[' + index + ']: ' + layerName + ' not recognized layer type');
   }
 }
 
@@ -264,32 +262,32 @@ function addLayer(assetName, index, map) {
  * should be able to set its visibility and redraw the layers.
  *
  * @param {Promise<Array<GeoJson>>}featuresPromise
- * @param {string} assetName
+ * @param {string} layerName
  * @param {number} index Ordering of layer (higher is more visible)
  */
-function addLayerFromGeoJsonPromise(featuresPromise, assetName, index) {
+function addLayerFromGeoJsonPromise(featuresPromise, layerName, index) {
   addLoadingElement(mapContainerId);
   // Add entry to map.
   const layerMapValue = new LayerMapValue(null, index, true);
-  layerMap.set(assetName, layerMapValue);
+  layerMap.set(layerName, layerMapValue);
   featuresPromise
       .then((features) => {
         layerMapValue.data = features;
-        addLayerFromFeatures(layerMapValue, assetName);
+        addLayerFromFeatures(layerMapValue, layerName);
         loadingElementFinished(mapContainerId);
       })
-      .catch(createError('Error rendering ' + assetName));
+      .catch(createError('Error rendering ' + layerName));
 }
 
 /**
  * Adds an entry to layerMap when we haven't actually gotten the data yet.
- * Useful for assets that we don't want to display by default.
+ * Useful for layers that we don't want to display by default.
  *
- * @param {string} assetName
+ * @param {string} layerName
  * @param {number} index
  */
-function addNullLayer(assetName, index) {
-  layerMap.set(assetName, new LayerMapValue(null, index, false));
+function addNullLayer(layerName, index) {
+  layerMap.set(layerName, new LayerMapValue(null, index, false));
 }
 
 /**
@@ -314,22 +312,38 @@ function redrawLayers() {
  * Removes an entry from the map by setting its displayed attribute to false and
  * recreating the layer.
  *
- * @param {string} assetName
+ * @param {string} layerName
  * @param {google.maps.Map} map main map
  */
-function removeLayer(assetName, map) {
-  switch (assets[assetName] && assets[assetName].getType()) {
-    case EarthEngineAsset.Type.IMAGE:
-    case EarthEngineAsset.Type.IMAGECOLLECTION:
-      map.overlayMapTypes.setAt(layerMap[assetName].index, null);
-      layerMap[assetName].displayed = false;
+function removeLayer(layerName, map) {
+  if (layerName === scoreLayerName) {
+    removeFeatureCollection(layerName);
+    return;
+  }
+  switch (firebaseLayers[layerName]['asset-type']) {
+    case LayerType.IMAGE:
+    case LayerType.IMAGE_COLLECTION:
+      map.overlayMapTypes.setAt(layerMap[layerName].index, null);
+      layerMap[layerName].displayed = false;
+      break;
+    case LayerType.FEATURE:
+    case LayerType.FEATURE_COLLECTION:
+      removeFeatureCollection(layerName);
       break;
     default:
-      const layerMapValue = layerMap.get(assetName);
-      layerMapValue.displayed = false;
-      addLayerFromFeatures(layerMapValue, assetName);
-      break;
+      createError('parsing layer type during remove')(
+          '[' + index + ']: ' + layerName + ' not recognized layer type');
   }
+}
+
+/**
+ * convenience function for removeLayer on feature collections.
+ * @param {string} layerName
+ */
+function removeFeatureCollection(layerName) {
+  const layerMapValue = layerMap.get(layerName);
+  layerMapValue.displayed = false;
+  addLayerFromFeatures(layerMapValue, layerName);
 }
 
 /**

cypress/integration/unit_tests/style_functions_test.js
@@ -1,0 +1,69 @@
+import {firebaseLayers} from '../../../docs/firebase_layers';
+import {colorMap, getStyleFunction, initializeFirebaseLayers} from '../../../docs/firebase_layers.js';
+
+describe('Unit test for generating style functions', () => {
+  before(() => initializeFirebaseLayers({}));
+
+  afterEach(
+      () => Object.keys(firebaseLayers)
+                .forEach((asset) => firebaseLayers[asset] = undefined));
+
+  it('calculates a discrete function', () => {
+    firebaseLayers['asset0'] = {
+      'color-function': {
+        'continuous': false,
+        'field': 'flavor',
+
+        'opacity': 100,
+        'colors': {
+          'cherry': 'red',
+          'banana': 'yellow',
+        },
+      },
+    };
+
+    const fxn = getStyleFunction('asset0');
+    const cherry = fxn({'properties': {'flavor': 'cherry'}});
+    const expectedCherry = colorMap.get('red');
+    expect(cherry).to.eql(expectedCherry);
+    const banana = fxn({'properties': {'flavor': 'banana'}});
+    const expectedBanana = colorMap.get('yellow');
+    expect(banana).to.eql(expectedBanana);
+  });
+
+  it('calculates a continuous function', () => {
+    firebaseLayers['asset1'] = {
+      'color-function': {
+        'continuous': true,
+        'field': 'oranges',
+        'base-color': 'orange',
+        'opacity': 83,
+        'min': 14,
+        'max': 10005,
+      },
+    };
+
+    const fxn = getStyleFunction('asset1');
+    const orangeish = fxn({'properties': {'oranges': 734}});
+    // orange = [255, 140, 0]
+    // white = [255, 255, 255]
+    expect(orangeish[0]).to.eq((255 * (734 - 14) + 255 * (10005 - 734)) / 2);
+    expect(orangeish[1]).to.eq((140 * (734 - 14) + 255 * (10005 - 734)) / 2);
+    expect(orangeish[2]).to.eq((0 * (734 - 14) + 255 * (10005 - 734)) / 2);
+    expect(orangeish[3]).to.eq(83);
+  });
+
+  it('calculates a single-color function', () => {
+    firebaseLayers['asset2'] = {
+      'color-function': {
+        'single-color': 'blue',
+        'opacity': 83,
+      },
+    };
+
+    const fxn = getStyleFunction('asset2');
+    const trueBlue = fxn({});
+    const blue = colorMap.get('blue');
+    expect(trueBlue).to.eql(blue);
+  });
+});

cypress/integration/unit_tests/checkbox_test.js
@@ -1,8 +1,29 @@
-import {assets, EarthEngineAsset} from '../../../docs/earth_engine_asset.js';
-import {layerArray, layerMap, LayerMapValue, setMapToDrawLayersOn, toggleLayerOff, toggleLayerOn} from '../../../docs/layer_util';
-import {loadScriptsBefore} from '../../support/script_loader';
+import {initializeFirebaseLayers, LayerType} from '../../../docs/firebase_layers.js';
+import {layerArray, layerMap, LayerMapValue, setMapToDrawLayersOn, toggleLayerOff, toggleLayerOn} from '../../../docs/layer_util.js';
+import {loadScriptsBefore} from '../../support/script_loader.js';
 
 const mockData = {};
+
+const mockFirebaseLayers = {
+  'asset0': {
+    'asset-type': LayerType.FEATURE_COLLECTION,
+    'display-name': 'asset0',
+    'display-on-load': true,
+    'color-function': {'single-color': 'yellow'},
+  },
+  'asset1': {
+    'asset-type': LayerType.FEATURE_COLLECTION,
+    'display-name': 'asset1',
+    'display-on-load': false,
+    'color-function': {'single-color': 'yellow'},
+  },
+  'asset2': {
+    'asset-type': LayerType.FEATURE_COLLECTION,
+    'display-name': 'asset2',
+    'display-on-load': false,
+    'color-function': {'single-color': 'yellow'},
+  },
+};
 
 describe('Unit test for toggleLayerOn', () => {
   loadScriptsBefore('ee', 'deck', 'maps');
@@ -10,12 +31,7 @@ describe('Unit test for toggleLayerOn', () => {
     layerMap.set('asset0', new LayerMapValue(mockData, 0, true));
     layerMap.set('asset1', new LayerMapValue(mockData, 1, false));
     layerMap.set('asset2', new LayerMapValue(null, 2, false));
-    assets['asset0'] = new EarthEngineAsset(
-        EarthEngineAsset.Type.FEATURECOLLECTION, 'asset0', true);
-    assets['asset1'] = new EarthEngineAsset(
-        EarthEngineAsset.Type.FEATURECOLLECTION, 'asset1', false);
-    assets['asset2'] = new EarthEngineAsset(
-        EarthEngineAsset.Type.FEATURECOLLECTION, 'asset2', false);
+    initializeFirebaseLayers(mockFirebaseLayers);
     // Initialize deck object in production.
     setMapToDrawLayersOn(null);
     layerArray[0] = new deck.GeoJsonLayer({});

commit fdf735b5fb54b294b021b70e848555d4106259bb

Fix color rendering (#449)
Don't accidentally modify the color arrays.

I wonder if the latest deck.gl that we started using a few weeks before fellowship ended got stricter? It looks like this bug has been here since this code was introduced in #186.

docs/firebase_layers.js
@@ -5,6 +5,7 @@ export {
   createStyleFunction,
   getLinearGradient,
   LayerType,
+  SOLID_BLACK,
 };
 
 const LayerType = {
@@ -24,7 +25,9 @@ const ColorStyle = {
 };
 Object.freeze(ColorStyle);
 
-const opacity = 200;
+const OPACITY = 200;
+
+const SOLID_BLACK = [0, 0, 0];
 
 /**
  * Creates the style function for the given properties. Caller should cache to
@@ -77,7 +80,7 @@ function createContinuousFunction(field, minVal, maxVal, color) {
       rgba.push(
           white[i] + (colorRgb[i] - white[i]) * ((value - minVal) / range));
     }
-    rgba.push(opacity);
+    rgba.push(OPACITY);
     return rgba;
   };
 }
@@ -93,24 +96,22 @@ function createDiscreteFunction(field, colors) {
   return (feature) => {
     const value = feature['properties'][field];
     if (value === null) return transparent;
-    const rgba = colorMap.get(colors[value]);
-    rgba.push(opacity);
-    return rgba;
+    return colorMap.get(colors[value]);
   };
 }
 
-const transparent = [0, 0, 0, 0];
+const transparent = Object.freeze([0, 0, 0, 0]);
 
-const colorMap = new Map([
-  ['red', [255, 0, 0]],
-  ['orange', [255, 140, 0]],
-  ['yellow', [255, 255, 0]],
-  ['green', [0, 255, 0]],
-  ['blue', [0, 0, 255]],
-  ['purple', [128, 0, 128]],
-  ['black', [0, 0, 0]],
-  ['white', [255, 255, 255]],
-]);
+const colorMap = Object.freeze(new Map([
+  ['red', Object.freeze([255, 0, 0, OPACITY])],
+  ['orange', Object.freeze([255, 140, 0, OPACITY])],
+  ['yellow', Object.freeze([255, 255, 0, OPACITY])],
+  ['green', Object.freeze([0, 255, 0, OPACITY])],
+  ['blue', Object.freeze([0, 0, 255, OPACITY])],
+  ['purple', Object.freeze([128, 0, 128, OPACITY])],
+  ['black', Object.freeze([0, 0, 0, OPACITY])],
+  ['white', Object.freeze([255, 255, 255, OPACITY])],
+]));
 
 /**
  * Gets the linear gradient of the colors for the legend.

docs/layer_util.js
@@ -4,7 +4,7 @@ import {mapContainerId} from './dom_constants.js';
 import {terrainStyle} from './earth_engine_asset.js';
 import {AssetNotFoundError, getEePromiseForFeatureCollection, transformEarthEngineFailureMessage} from './ee_promise_cache.js';
 import {showError} from './error.js';
-import {colorMap, createStyleFunction, LayerType} from './firebase_layers.js';
+import {createStyleFunction, LayerType, SOLID_BLACK} from './firebase_layers.js';
 import {addLoadingElement, loadingElementFinished} from './loading.js';
 
 export {
@@ -453,7 +453,7 @@ function addLayerFromFeatures(layerDisplayData, index) {
  * @return {Array} RGBA color specification as an array
  */
 function showColor(color) {
-  return color ? color : colorMap.get('black');
+  return color ? color : SOLID_BLACK;
 }
 
 /**

cypress/integration/unit_tests/style_functions_test.js
@@ -1,4 +1,4 @@
-import {colorMap, createStyleFunction} from '../../../docs/firebase_layers.js';
+import {createStyleFunction} from '../../../docs/firebase_layers.js';
 
 describe('Unit test for generating style functions', () => {
   it('calculates a discrete function', () => {
@@ -11,11 +11,13 @@ describe('Unit test for generating style functions', () => {
       },
     });
     const cherry = fxn({'properties': {'flavor': 'cherry'}});
-    const expectedCherry = colorMap.get('red');
+    const expectedCherry = [255, 0, 0, 200];
     expect(cherry).to.eql(expectedCherry);
     const banana = fxn({'properties': {'flavor': 'banana'}});
-    const expectedBanana = colorMap.get('yellow');
+    const expectedBanana = [255, 255, 0, 200];
     expect(banana).to.eql(expectedBanana);
+    const secondBanana = fxn({'properties': {'flavor': 'banana'}});
+    expect(secondBanana).to.eql(expectedBanana);
   });
 
   it('calculates a continuous function', () => {
@@ -40,7 +42,7 @@ describe('Unit test for generating style functions', () => {
       'color': 'blue',
     });
     const trueBlue = fxn({});
-    const blue = colorMap.get('blue');
+    const blue = [0, 0, 255, 200];
     expect(trueBlue).to.eql(blue);
   });
 });