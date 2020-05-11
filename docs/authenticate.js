givedirectly/Google-Partnership

commit 72dd1775cf549bec616f39a6467d73d3aaa2f42d

Update EarthEngine version to latest (#437)
EarthEngine now requests additional permissions, which Google.com corp accounts are not allowed to grant to third-party apps. As a workaround, use the token server if we get an error indicating that authentication failed due to a permissions error like this.

Images are loaded a bit differently now (`ee.MapLayerOverlay` is apparently deprecated), so had to modify layer_util.js. Is cleaner now.

Also noticed that after a minor version node upgrade (12.15-16), our tests started failing, so pinning at node v12.15. I think 12.16.1 should fix, but not important. More important is emailing test failures to GD when they happen.

docs/authenticate.js
@@ -80,7 +80,18 @@ class Authenticator {
    *     is finished
    */
   start() {
-    this.eeAuthenticate(() => this.navigateToSignInPage());
+    this.eeAuthenticate((err) => {
+      if (err === 'server_error') {
+        // EarthEngine now requests full Google Compute Engine access. Some
+        // organizations (Google.com, for instance) will not allow their users
+        // to grant this permission. Use the token server in that case.
+        showToastMessage(
+            'Error authenticating to EarthEngine, trying anonymous access', -1);
+        this.getEeTokenAndInitialize();
+      } else {
+        this.navigateToSignInPage();
+      }
+    });
     const gapiSettings = Object.assign({}, gapiTemplate);
     // TODO(janakr): We could make do with read-only access, but GD user needs
     //  to have full access, and we don't know at this stage whether it's the GD
@@ -156,14 +167,23 @@ class Authenticator {
             'Not whitelisted for EarthEngine access: ' +
                 'trying anonymous access',
             -1);
-        this.getAndSetEeTokenWithErrorHandling().then(() => {
-          showToastMessage('Anonymous access successful', 1000);
-          initializeEE(this.eeInitializeCallback, defaultErrorCallback);
-        });
+        this.getEeTokenAndInitialize();
       } else {
         defaultErrorCallback(err);
       }
     });
+  }
+
+  /**
+   * Calls {@link getAndSetEeTokenWithErrorHandling}, then informs user of
+   * success and calls ee.initialize now that we have a token.
+   * @return {Promise<void>} Promise that resolves when token has been set and
+   *     initialization has started
+   */
+  async getEeTokenAndInitialize() {
+    await this.getAndSetEeTokenWithErrorHandling();
+    showToastMessage('Anonymous access successful', 1000);
+    initializeEE(this.eeInitializeCallback, defaultErrorCallback);
   }
 
   /**
@@ -172,49 +192,42 @@ class Authenticator {
    * server so server can verify these aren't totally anonymous requests.
    * @return {Promise<void>} Promise that resolves when token has been set
    */
-  getAndSetEeTokenWithErrorHandling() {
-    // To get here, we must already have logged into Google via gapi, even if
-    // not with an EE-enabled account, so Google user id token available.
+  async getAndSetEeTokenWithErrorHandling() {
+    // Make sure we're logged in. If there is no login, user will have been
+    // redirected to sign-in page.
+    await this.gapiInitDone.getPromise();
     const idToken = gapi.auth2.getAuthInstance()
                         .currentUser.get()
                         .getAuthResponse()
                         .id_token;
-    return fetch(TOKEN_SERVER_URL, {
-             method: 'POST',
-             body: $.param({idToken}),
-             headers: {'Content-type': 'application/x-www-form-urlencoded'},
-           })
-        .then((response) => {
-          if (!response.ok) {
-            const message = 'Refresh token error: ' + response.status;
-            console.error(message, response);
-            const contact = CONTACT ? CONTACT : OWNER;
-            // TODO(#395): Find GD contact to list here.
-            alert(
-                'Error contacting server for access without EarthEngine ' +
-                'whitelisting. Please reload page and log in with an ' +
-                'EarthEngine-whitelisted account or contact ' + contact + ' ' +
-                'with error from JavaScript console.');
-            throw new Error(message);
-          }
-          return response.json();
-        })
-        .then(
-            ({accessToken, expireTime}) =>
-                new Promise(
-                    (resolve) => ee.data.setAuthToken(
-                        CLIENT_ID, 'Bearer', accessToken,
-                        Math.floor(
-                            getMillisecondsToDateString(expireTime) / 1000),
-                        /* extraScopes */[], resolve,
-                        /* updateAuthLibrary */ false))
-                    .then(
-                        () => setTimeout(
-                            () => this.getAndSetEeTokenWithErrorHandling(),
-                            Math.max(
-                                getMillisecondsToDateString(expireTime) -
-                                    TOKEN_EXPIRE_BUFFER,
-                                0))));
+    const response = await fetch(TOKEN_SERVER_URL, {
+      method: 'POST',
+      body: $.param({idToken}),
+      headers: {'Content-type': 'application/x-www-form-urlencoded'},
+    });
+    if (!response.ok) {
+      const message = 'Refresh token error: ' + response.status;
+      console.error(message, response);
+      const contact = CONTACT ? CONTACT : OWNER;
+      alert(
+          'Error contacting server for access without EarthEngine ' +
+          'whitelisting. Please reload page and log in with an ' +
+          'EarthEngine-whitelisted account or contact ' + contact + ' with ' +
+          'error from JavaScript console.');
+      showError('Error contacting server for anonymous EarthEngine access');
+      throw new Error(message);
+    }
+    const {accessToken, expireTime} = response.json();
+    await new Promise(
+        (resolve) => ee.data.setAuthToken(
+            CLIENT_ID, 'Bearer', accessToken,
+            Math.floor(getMillisecondsToDateString(expireTime) / 1000),
+            /* extraScopes */[], resolve,
+            /* updateAuthLibrary */ false));
+    setTimeout(
+        () => this.getAndSetEeTokenWithErrorHandling(),
+        Math.max(
+            getMillisecondsToDateString(expireTime) - TOKEN_EXPIRE_BUFFER, 0));
   }
 }
 
@@ -234,7 +247,6 @@ function trackEeAndFirebase(
     taskAccumulator, needsGdUser = false, additionalScopes = []) {
   let authenticator;
   const eeInitializeCallback = () => {
-    ee.data.setCloudApiEnabled(true);
     taskAccumulator.taskCompleted();
     if (!authenticator) {
       return;

.travis.yml
@@ -1,6 +1,6 @@
 language: node_js
 node_js:
-  - 12
+  - 12.15
 # Dist needed for services: xvfb, which allows us to run Chromium in a "GUI".
 dist: xenial
 services:

cypress/integration/unit_tests/checkbox_test.js
@@ -305,8 +305,10 @@ describe('Unit test for toggleLayerOn', () => {
         })
         .then(() => {
           expect(loadingFinishedStub).to.be.calledOnce;
+          expect(map.overlayMapTypes).to.have.length(4);
           overlay = map.overlayMapTypes.getAt(3);
-          expect(overlay).is.not.null;
+          expect(overlay).to.not.be.null;
+          expect(overlay).to.not.be.undefined;
           // Turn layer off: disappears from map.
           toggleLayerOff(3, map);
           expect(map.overlayMapTypes.getAt(3)).is.null;

cypress/integration/unit_tests/polygon_draw_test.js
@@ -384,7 +384,8 @@ describe('Unit test for ShapeData', () => {
     pressButtonAndWaitForPromise('save').then(() => {
       expect(this.spyResult.popupPendingCalculationSpy).to.be.calledOnce;
       expect(this.spyResult.popupCalculatedDataSpy).to.be.calledOnce;
-      expect(this.spyResult.eeSpy).to.be.calledOnce;
+      // Can't assert on number of calls since EE makes calls under the covers.
+      expect(this.spyResult.eeSpy).to.be.called;
       expect(this.spyResult.firestoreSpy).to.be.calledOnce;
     });
     assertOnFirestoreAndPopup(newPath);

cypress/plugins/main.js
@@ -106,7 +106,7 @@ function onFunction(on, config) {
     },
 
     getEarthEngineToken() {
-      return generateEarthEngineToken().then((data) => data.accessToken);
+      return generateEarthEngineToken(false).then((data) => data.accessToken);
     },
 
     /**

cypress/support/script_loader.js
@@ -37,8 +37,7 @@ const scriptMap = new Map([
   [
     'ee',
     {
-      script:
-          'https://rawcdn.githack.com/google/earthengine-api/3bb86bfc4f3d9eed98220f3d225b414982915b86/javascript/build/ee_api_js_debug.js',
+      script: LIB_URL + 'ee_api_js_debug-0.1.214.js',
       callback: () => typeof (ee) !== 'undefined',
     },
   ],

docs/composite_image_map_type.js
@@ -179,9 +179,14 @@ class CompositeImageMapType {
   maybeSendEvent(adding) {
     if (!this.tileMap.size) {
       const event = new Event('tile');
-      event.count = adding ? 1 : 0;
+      event.loadingTileCount = adding ? 1 : 0;
       this.eventTarget.dispatchEvent(event);
     }
+  }
+
+  /** @return {number} The number of currently loading tiles */
+  getLoadingTilesCount() {
+    return this.tileMap.size;
   }
 }
 

docs/ee_test.html
@@ -1,6 +1,6 @@
 <html>
 <head>
-  <script src="external_libs/ee_api_js_debug-0.1.210.js"></script>
+  <script src="external_libs/ee_api_js_debug-0.1.214.js"></script>
 </head>
 <body>
 <script>

commit 82f0a311186c94dd09c3bdb84642a66f2a359948

Properly wait for response.json() promise (#452)
* Properly wait for response.json() promise.

Broken by 72dd177#diff-10d6fe37a932cbb977d8ff39d67db610L203: we used to chain promises. When I switched to async style as part of EE upgrade, forgot to add "await" here.

docs/authenticate.js
@@ -217,7 +217,7 @@ class Authenticator {
       showError('Error contacting server for anonymous EarthEngine access');
       throw new Error(message);
     }
-    const {accessToken, expireTime} = response.json();
+    const {accessToken, expireTime} = await response.json();
     await new Promise(
         (resolve) => ee.data.setAuthToken(
             CLIENT_ID, 'Bearer', accessToken,

token_server/token_server.js
@@ -104,6 +104,7 @@ function generateTokenPeriodically() {
   createServer(async (req, res) => {
     const origin = req.headers['origin'];
     if (!allowedOrigins.has(origin)) {
+      console.warn('Bad origin', origin);
       fail(res);
       return;
     }
@@ -112,6 +113,7 @@ function generateTokenPeriodically() {
       const {idToken} = await parseBody(req);
       await client.verifyIdToken({idToken: idToken, audience: CLIENT_ID});
     } catch (err) {
+      console.warn('Error verifying id token', err, idToken);
       fail(res);
       return;
     }