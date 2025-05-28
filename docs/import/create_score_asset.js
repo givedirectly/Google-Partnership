54016116946f833474ac38f546018f5e7e4fcfc6
Janak Ramakrishnan <janakr@google.com>
2025-05-28 14:10:50 -0400

    format

--- docs/import/create_score_asset.js
+++ docs/import/create_score_asset.js
@@ -8,12 +8,8 @@ import {computeAndSaveBounds} from './center.js';
 import {BUILDING_COUNT_KEY, BuildingSource} from './create_disaster_lib.js';
 import {backupCensusBlockGroupKey, backupCensusGeoidKey, backupIncomeKey, backupSnapKey, backupTotalKey, cdcGeoidKey, censusBlockGroupKey, censusGeoidKey, incomeKey, snapKey, sviKey, tigerGeoidKey, totalKey} from './state_based_key_names.js';
 
-export {
-  createScoreAssetForFlexibleDisaster,
-  createScoreAssetForStateBasedDisaster,
-  renameProperty,
-  setStatus
-};
+export {createScoreAssetForFlexibleDisaster, createScoreAssetForStateBasedDisaster, setStatus};
+
 // clang-format off
 // @VisibleForTesting
 export {backUpAssetAndStartTask, renameProperty};
